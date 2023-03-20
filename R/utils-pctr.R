e3083840d44186589b5e6da12d7d2c9c99b045b3
Mauro Lepore <maurolepore@gmail.com>
2023-03-20 00:55:27 +0000

    Update namespace

--- R/utils-pctr.R
+++ R/utils-pctr.R
@@ -1,4 +1,14 @@
-pctr_company_scores <- function(path = here::here("vignettes/articles/pctr.Rmd")) {
+#' Product carbon transition risk indicator
+#'
+#' @param path Path to article.
+#'
+#' @return A data frame.term
+#'
+#' @export
+#'
+#' @examples
+#' pctr()
+pctr <- function(path = here::here("vignettes/articles/pctr.Rmd")) {
   wrap_rmd(path)[["company_scores"]]
 }
 
