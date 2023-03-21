6c240fd915ec5fa99d03424f6c869dc560cc18e3
Mauro Lepore <maurolepore@gmail.com>
2023-03-21 18:02:18 +0000

    Polish docs and tests

--- tests/testthat/test-wrap_rmd.R
+++ tests/testthat/test-wrap_rmd.R
@@ -1,5 +1,6 @@
 test_that("outputs a named list of one tibble element per output.csv", {
-  out <- wrap_rmd(test_path("data", "mtcars.Rmd"))
+  rmd <- system.file("extdata", "mtcars.Rmd", package = "tiltIndicator")
+  out <- wrap_rmd(rmd)
   expect_type(out, "list")
   expect_named(out, "mtcars")
   expect_equal(out[["mtcars"]], tibble::as_tibble(mtcars))
