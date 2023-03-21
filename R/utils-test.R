3827538a44518ce1b8f09115070acc9d637266dd
Mauro Lepore <maurolepore@gmail.com>
2023-03-21 20:20:55 +0000

    Extend the family of dev-oriented functions

--- R/utils-test.R
+++ R/utils-test.R
@@ -1,3 +1,26 @@
+#' Run private tests
+#'
+#' @family developer-oriented functions
+#' @return
+#' @export
+#' @examples
+#' \dontrun{
+#' test_private()
+#' }
 test_private <- function() {
   testthat::test_dir(tilt_path("tiltIndicator", "tests"))
+}
+
+#' Update snapshots in the directory for private tests
+#'
+#' @inheritParams testthat::snapshot_accept
+#' @family developer-oriented functions
+#' @return
+#' @export
+#' @examples
+#' \dontrun{
+#' snapshot_accept_private()
+#' }
+snapshot_accept_private <- function(files = NULL) {
+  testthat::snapshot_accept(files = NULL, path = tilt_path("tiltIndicator", "tests"))
 }
