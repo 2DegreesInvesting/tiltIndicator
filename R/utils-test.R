ec5fd1b8b4c861961bd29f924e8be30050c3992a
Mauro Lepore <maurolepore@gmail.com>
2023-03-22 14:15:20 -0300

    Dont expor test utils (#10)
    * Extend the family of dev-oriented functions
    
    * Don't export test-utils

--- R/utils-test.R
+++ R/utils-test.R
@@ -1,3 +1,26 @@
+#' Run private tests
+#'
+#' @family developer-oriented functions
+#' @return Called for its side effect.
+#' @examples
+#' \dontrun{
+#' test_private()
+#' }
+#' @noRd
 test_private <- function() {
   testthat::test_dir(tilt_path("tiltIndicator", "tests"))
+}
+
+#' Update snapshots in the directory for private tests
+#'
+#' @inheritParams testthat::snapshot_accept
+#' @family developer-oriented functions
+#' @return Called for its side effect.
+#' @examples
+#' \dontrun{
+#' snapshot_accept_private()
+#' }
+#' @noRd
+snapshot_accept_private <- function(files = NULL) {
+  testthat::snapshot_accept(files = NULL, path = tilt_path("tiltIndicator", "tests"))
 }
