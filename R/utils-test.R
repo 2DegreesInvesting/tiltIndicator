6edd6215ed7bdd65c635f30bbf3856d695d4455c
Mauro Lepore <maurolepore@gmail.com>
2023-03-22 17:08:53 +0000

    Don't export test-utils

--- R/utils-test.R
+++ R/utils-test.R
@@ -1,12 +1,12 @@
 #' Run private tests
 #'
 #' @family developer-oriented functions
-#' @return
-#' @export
+#' @return Called for its side effect.
 #' @examples
 #' \dontrun{
 #' test_private()
 #' }
+#' @noRd
 test_private <- function() {
   testthat::test_dir(tilt_path("tiltIndicator", "tests"))
 }
@@ -15,12 +15,12 @@ test_private <- function() {
 #'
 #' @inheritParams testthat::snapshot_accept
 #' @family developer-oriented functions
-#' @return
-#' @export
+#' @return Called for its side effect.
 #' @examples
 #' \dontrun{
 #' snapshot_accept_private()
 #' }
+#' @noRd
 snapshot_accept_private <- function(files = NULL) {
   testthat::snapshot_accept(files = NULL, path = tilt_path("tiltIndicator", "tests"))
 }
