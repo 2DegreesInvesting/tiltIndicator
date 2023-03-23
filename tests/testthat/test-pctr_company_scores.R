9e948b0d77ede86041bbdcbe61ce66afc383da22
Mauro Lepore <maurolepore@gmail.com>
2023-03-23 19:23:55 +0000

    Test that the reviewed versoin is the same

--- tests/testthat/test-pctr_company_scores.R
+++ tests/testthat/test-pctr_company_scores.R
@@ -1,14 +1,32 @@
 test_that("hasn't changed", {
   skip_on_ci()
-
   testthat::local_edition(3)
   set.seed(123)
 
   options(readr.show_col_types = FALSE)
   input1 <- readr::read_csv(pctr_path("input_data", "ecoinvent_activities.csv"))
   input2 <- readr::read_csv(pctr_path("input_data", "MVP_sample_dataset.csv"))
-  out <- pctr_company_scores(input1, input2)
+  mvp <- mvp_path("product-carbon-transition-risk.Rmd")
+  out <- pctr_company_scores(input1, input2, mvp = mvp)
   out <- format_robust_snapshot(out)
 
   expect_snapshot(out)
+})
+
+test_that("hasn't changed", {
+  skip_on_ci()
+  testthat::local_edition(3)
+  set.seed(123)
+
+  options(readr.show_col_types = FALSE)
+  input1 <- readr::read_csv(pctr_path("input_data", "ecoinvent_activities.csv"))
+  input2 <- readr::read_csv(pctr_path("input_data", "MVP_sample_dataset.csv"))
+
+  mvp1 <- mvp_path("product-carbon-transition-risk_review.Rmd")
+  out1 <- pctr_company_scores(input1, input2, mvp = mvp1)
+
+  mvp2 <- mvp_path("product-carbon-transition-risk_review.Rmd")
+  out2 <- pctr_company_scores(input1, input2, mvp = mvp2)
+
+  expect_equal(out1, out2)
 })
