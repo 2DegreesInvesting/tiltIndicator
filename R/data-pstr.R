76b64bfa62ba9310d187b6aa3e5a02fa11da7ab0
Mauro Lepore <maurolepore@gmail.com>
2023-03-24 01:14:32 +0000

    Document data

---
+++ R/data-pstr.R
@@ -0,0 +1,17 @@
+#' Fake dataset of some companies and their products
+#'
+#' @examples
+#' companies
+"companies"
+
+#' WEO 2022
+#'
+#' @examples
+#' weo_2022
+"weo_2022"
+
+#' Mapper between europages and WEO sectors
+#'
+#' @examples
+#' ep_weo
+"ep_weo"
