eb34104f375173c51be0f8485793ebdac39f0878
Mauro Lepore <maurolepore@gmail.com>
2023-03-24 16:41:55 +0000

    Refactor names

--- tests/testthat/test-pstr.R
+++ tests/testthat/test-pstr.R
@@ -1,6 +1,6 @@
 test_that("`with_reductions` remains the same", {
   path <- mvp_path("product-sector-transition-risk.Rmd")
-  with_reductions <- render_list(path)$final_df
+  with_reductions <- render_list(path)$with_reductions
   out <- format_robust_snapshot(with_reductions)
 
   expect_snapshot(out)
@@ -8,14 +8,14 @@ test_that("`with_reductions` remains the same", {
 
 test_that("`with_transition_risk` remains the same", {
   path <- mvp_path("product-sector-transition-risk.Rmd")
-  with_transition_risk <- render_list(path)$final_df_cat
+  with_transition_risk <- render_list(path)$with_transition_risk
   out <- format_robust_snapshot(with_transition_risk)
   expect_snapshot(out)
 })
 
 test_that("`with_score_aggregated` remains the same", {
   path <- mvp_path("product-sector-transition-risk.Rmd")
-  with_score_aggregated <- render_list(path)$aggregated_score_df
+  with_score_aggregated <- render_list(path)$with_score_aggregated
   out <- format_robust_snapshot(with_score_aggregated)
   expect_snapshot(out)
 })
