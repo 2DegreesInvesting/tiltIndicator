8976da7f7897db836aca5d3718bd3deaa41220ca
Mauro Lepore <maurolepore@gmail.com>
2023-03-24 15:54:41 +0000

    Polish family name

--- R/ep_weo.R
+++ R/ep_weo.R
@@ -1,6 +1,6 @@
 #' Mapper between europages and WEO sectors
 #'
-#' @family datasets used in the product-sector-transition-risk indicator.
+#' @family datasets for product sector transition risk.
 #' @examples
 #' ep_weo
 "ep_weo"
