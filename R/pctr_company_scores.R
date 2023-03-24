c0f898f73808f0a92fbca117abc2d7863744571c
Mauro Lepore <maurolepore@gmail.com>
2023-03-24 13:46:46 +0000

    Remove some

--- R/pctr_company_scores.R
+++
@@ -1,5 +0,0 @@
-pctr_company_scores <- function(..., mvp = mvp_path("product-carbon-transition-risk.Rmd")) {
-  dots <- rlang::quos_auto_name(rlang::enquos(...))
-  params <- rlang::set_names(list(...), names(dots))
-  wrap_rmd(path = mvp, params = params)[["company_scores"]]
-}
--- tests/testthat/test-pctr_path.R
+++
@@ -1,3 +0,0 @@
-test_that("returns the expected directory structure", {
-  expect_true(grepl("Downloads/tilt/tiltIndicator/pctr", pctr_path()))
-})
