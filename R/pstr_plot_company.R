b092ef65a9cd76e892462ffa8259f35a6f8810ae
Mauro Lepore <maurolepore@gmail.com>
2023-03-24 20:20:25 +0000

    Move pstr_plot_company_()

---
+++ R/pstr_plot_company.R
@@ -0,0 +1,22 @@
+# TODO: Complete documentation
+#' Title
+#'
+#' @param data A data frame. The ouptput of [pstr_aggregate_scores()].
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' TODO: Move to R/function-name.R
+pstr_plot_company <- function(with_score_aggregated, company_name) {
+  .company_name <- company_name
+  level_order <- c("low", "medium", "high")
+
+  with_score_aggregated |>
+    filter(.data$company_name == .company_name) |>
+    ggplot(aes(x = transition_risk, y = score_aggregated, fill = score_aggregated)) +
+    scale_x_discrete(limits = level_order) +
+    geom_col() +
+    scale_fill_gradient2(midpoint = 0, low = "red", high = "blue") +
+    facet_grid(company_name ~ scenario + year)
+}
