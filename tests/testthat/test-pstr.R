05823b1c1eab3efa68e4a8704e2cf7f9405652ae
Mauro Lepore <maurolepore@gmail.com>
2023-03-24 00:41:32 +0000

    Snapshot

--- tests/testthat/test-pstr.R
+++ tests/testthat/test-pstr.R
@@ -1,4 +1,23 @@
-test_that("capture outputs", {
+test_that("`with_reductions` remains the same", {
   mvp <- mvp_path("product-sector-transition-risk.Rmd")
   out <- wrap_rmd(mvp)
+  out <- out[["with_reductions"]]
+  out <- format_robust_snapshot(out)
+  expect_snapshot(out)
+})
+
+test_that("`with_transition_risk` remains the same", {
+  mvp <- mvp_path("product-sector-transition-risk.Rmd")
+  out <- wrap_rmd(mvp)
+  out <- out[["with_transition_risk"]]
+  out <- format_robust_snapshot(out)
+  expect_snapshot(out)
+})
+
+test_that("`with_score_aggregated` remains the same", {
+  mvp <- mvp_path("product-sector-transition-risk.Rmd")
+  out <- wrap_rmd(mvp)
+  out <- out[["with_score_aggregated"]]
+  out <- format_robust_snapshot(out)
+  expect_snapshot(out)
 })
