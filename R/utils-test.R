4a188888491020aaec90e9da77cc5130afaea921
Mauro Lepore <maurolepore@gmail.com>
2023-03-26 18:34:54 +0000

    Prune

--- R/utils-test.R
+++ R/utils-test.R
@@ -1,30 +1,3 @@
-#' Run private tests
-#'
-#' @family developer-oriented functions
-#' @return Called for its side effect.
-#' @examples
-#' \dontrun{
-#' test_private()
-#' }
-#' @noRd
-test_private <- function() {
-  testthat::test_dir(tilt_path("tiltIndicator", "tests"))
-}
-
-#' Update snapshots in the directory for private tests
-#'
-#' @inheritParams testthat::snapshot_accept
-#' @family developer-oriented functions
-#' @return Called for its side effect.
-#' @examples
-#' \dontrun{
-#' snapshot_accept_private()
-#' }
-#' @noRd
-snapshot_accept_private <- function(files = NULL) {
-  testthat::snapshot_accept(files = NULL, path = tilt_path("tiltIndicator", "tests"))
-}
-
 #' Format data to print robust snapshots
 #'
 #' Prints each column separately, one under the other. This avoids differences
