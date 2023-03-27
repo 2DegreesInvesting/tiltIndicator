1ee5b21450a664b7dfad1a0a79dfa418b49c1408
delacombazlinda <delacombaz.linda@gmail.com>
2023-03-27 16:24:04 +0000

    documented pstr_plot_company.R

--- R/pstr_plot_company.R
+++ R/pstr_plot_company.R
@@ -1,13 +1,34 @@
-#' Title
+#' Plot the product sector transition risk for a company
+#'
+#' @description
+#' Outputs a plot on which the x-axis is the product sector
+#' transition risk and the y-axis is the aggregated product score in % for
+#' the company `company_name`.
 #'
 #' @param data A data frame. The output of [pstr_aggregate_scores()].
-#' @param company_name TODO
+#' @param company_name A string. Name of the company in the portfolio that
+#' the user wants to plot.
 #'
-#' @return TODO
+#' @return A [ggplot()].
 #' @export
 #'
 #' @examples
-#' # TODO
+#'
+#'library(tibble)
+#'
+#'data <- tibble(
+#'          company_name = "a",
+#'          transition_risk = c("high","low"),
+#'          scenario = c("A", "B"),
+#'          year = 2020,
+#'          score_aggregated = 50
+#'          )
+#'data
+#'
+#'company_name <- "a"
+#'company_name
+#'
+#'pstr_plot_company(data, company_name)
 pstr_plot_company <- function(data, company_name) {
   level_order <- c("low", "medium", "high")
 
