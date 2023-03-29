c9bda2b47cadcbdaaf611e8dd7d48902d9742593
Mauro Lepore <maurolepore@gmail.com>
2023-03-29 23:51:09 +0000

    snap

--- tests/testthat/test-pctr.R
+++ tests/testthat/test-pctr.R
@@ -4,3 +4,10 @@ test_that("`ecoinvent_ranks` remains the same", {
   out <- format_robust_snapshot(ecoinvent_ranks)
   expect_snapshot(out)
 })
+
+test_that("`ecoinvent_scores` remains the same", {
+  path <- mvp_path("pctr.Rmd")
+  ecoinvent_scores <- render_to_list(path)$ecoinvent_scores
+  out <- format_robust_snapshot(ecoinvent_scores)
+  expect_snapshot(out)
+})
