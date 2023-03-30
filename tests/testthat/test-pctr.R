82a935f98e3be3d6cc9fbb8d97d050f390ecd451
Mauro Lepore <maurolepore@gmail.com>
2023-03-30 14:01:39 +0000

    refactor

--- tests/testthat/test-pctr.R
+++ tests/testthat/test-pctr.R
@@ -12,34 +12,6 @@ test_that("`ecoinvent_scores` remains the same", {
   expect_snapshot(out)
 })
 
-test_that("`companies_scores` remains the same", {
-  path <- mvp_path("pctr.Rmd")
-  companies_scores <- render_to_list(path)$companies_scores
-  out <- format_robust_snapshot(companies_scores)
-  expect_snapshot(out)
-})
-
-test_that("`scores_all` remains the same", {
-  path <- mvp_path("pctr.Rmd")
-  scores_all <- render_to_list(path)$scores_all
-  out <- format_robust_snapshot(scores_all)
-  expect_snapshot(out)
-})
-
-test_that("`scores_unit` remains the same", {
-  path <- mvp_path("pctr.Rmd")
-  scores_unit <- render_to_list(path)$scores_unit
-  out <- format_robust_snapshot(scores_unit)
-  expect_snapshot(out)
-})
-
-test_that("`scores_unit_sec` remains the same", {
-  path <- mvp_path("pctr.Rmd")
-  scores_unit_sec <- render_to_list(path)$scores_unit_sec
-  out <- format_robust_snapshot(scores_unit_sec)
-  expect_snapshot(out)
-})
-
 test_that("`pctr_output` remains the same", {
   path <- mvp_path("pctr.Rmd")
   pctr_output <- render_to_list(path)$pctr_output
