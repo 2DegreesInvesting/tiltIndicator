ee9174571d409500b960c0eb0e14d83508eee3f3
Mauro Lepore <maurolepore@gmail.com>
2023-03-30 11:42:28 +0000

    * New pctr data: pctr_ecoinvent_c02 and pctr_companies

---
+++ R/pctr_data.R
@@ -0,0 +1,14 @@
+#' TODO add title
+#'
+#' @source TODO add source
+#' @examples
+#' pctr_ecoinvent_c02
+"pctr_ecoinvent_c02"
+
+#' TODO add title
+#'
+#' @source TODO add source
+#' @examples
+#' pctr_companies
+"pctr_companies"
+
---
+++ data-raw/pctr_data.R
@@ -0,0 +1,16 @@
+library(readr)
+library(here)
+library(usethis)
+
+# Source: Tilman via tilt.indicator.product.carbon.tr-main_v2.zip at:
+# https://github.com/2DegreesInvesting/tiltData/issues/200#issue-1623506353
+pctr_ecoinvent_c02 <- here("data-raw/pctr/data_ecoinvent_co2.csv") |>
+  read_csv(show_col_types = FALSE)
+use_data(pctr_ecoinvent_c02, overwrite = TRUE)
+
+# Source: Tilman via tilt.indicator.product.carbon.tr-main_v2.zip at:
+# https://github.com/2DegreesInvesting/tiltData/issues/200#issue-1623506353
+
+pctr_companies <- here("data-raw/pctr/tilt_companies.csv") |>
+  read_csv(show_col_types = FALSE)
+use_data(pctr_companies, overwrite = TRUE)
