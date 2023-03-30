9bd746b68154380ad7addf95038944a3fe5ae8e6
Mauro Lepore <maurolepore@gmail.com>
2023-03-30 15:08:37 +0000

    refactor

--- tests/testthat/test-pctr.R
+++ tests/testthat/test-pctr.R
@@ -1,10 +1,3 @@
-test_that("`ecoinvent_ranks` remains the same", {
-  path <- mvp_path("pctr.Rmd")
-  ecoinvent_ranks <- render_to_list(path)$ecoinvent_ranks
-  out <- format_robust_snapshot(ecoinvent_ranks)
-  expect_snapshot(out)
-})
-
 test_that("`ecoinvent_scores` remains the same", {
   path <- mvp_path("pctr.Rmd")
   ecoinvent_scores <- render_to_list(path)$ecoinvent_scores
