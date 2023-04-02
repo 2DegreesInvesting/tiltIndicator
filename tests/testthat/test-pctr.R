35d807d135a2a8c1988a7e110f003a5b22178457
Mauro Lepore <maurolepore@gmail.com>
2023-04-02 15:09:23 +0000

    Wip

--- tests/testthat/test-pctr.R
+++ tests/testthat/test-pctr.R
@@ -1,10 +1,3 @@
-test_that("`scored_activities` remains the same", {
-  path <- mvp_path("pctr.Rmd")
-  scored_activities <- render_to_list(path)$scored_activities
-  out <- format_robust_snapshot(scored_activities)
-  expect_snapshot(out)
-})
-
 test_that("`scored_companies` remains the same", {
   path <- mvp_path("pctr.Rmd")
   scored_companies <- render_to_list(path)$scored_companies
