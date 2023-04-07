6a8c2891156d9894430f4014741054612c247464
Mauro Lepore <maurolepore@gmail.com>
2023-04-07 16:10:44 +0000

    Prune tests

--- tests/testthat/test-ictr.R
+++ tests/testthat/test-ictr.R
@@ -10,30 +10,6 @@ test_that("`ecoinvent_scores` hasn't changed", {
   expect_snapshot(out)
 })
 
-test_that("`scores_all` hasn't changed", {
-  scores_all <- render_to_list(mvp_path("ictr.Rmd"))$scores_all
-  out <- format_robust_snapshot(scores_all)
-  expect_snapshot(out)
-})
-
-test_that("`scores_unit` hasn't changed", {
-  scores_unit <- render_to_list(mvp_path("ictr.Rmd"))$scores_unit
-  out <- format_robust_snapshot(scores_unit)
-  expect_snapshot(out)
-})
-
-test_that("`scores_sector` hasn't changed", {
-  scores_sector <- render_to_list(mvp_path("ictr.Rmd"))$scores_sector
-  out <- format_robust_snapshot(scores_sector)
-  expect_snapshot(out)
-})
-
-test_that("`scores_unit_sec` hasn't changed", {
-  scores_unit_sec <- render_to_list(mvp_path("ictr.Rmd"))$scores_unit_sec
-  out <- format_robust_snapshot(scores_unit_sec)
-  expect_snapshot(out)
-})
-
 test_that("`pctr_output` hasn't changed", {
   pctr_output <- render_to_list(mvp_path("ictr.Rmd"))$pctr_output
   out <- format_robust_snapshot(pctr_output)
