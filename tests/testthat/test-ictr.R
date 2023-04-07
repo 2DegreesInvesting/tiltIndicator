243c1b4f581877e4eafa439aa01cdf190053904f
Mauro Lepore <maurolepore@gmail.com>
2023-04-07 18:41:07 +0000

    New ictr_score_inputs()

--- tests/testthat/test-ictr.R
+++ tests/testthat/test-ictr.R
@@ -1,9 +1,3 @@
-test_that("`ecoinvent_input` hasn't changed", {
-  ecoinvent_input <- render_to_list(mvp_path("ictr.Rmd"))$ecoinvent_input
-  out <- format_robust_snapshot(ecoinvent_input)
-  expect_snapshot(out)
-})
-
 test_that("`ecoinvent_scores` hasn't changed", {
   ecoinvent_scores <- render_to_list(mvp_path("ictr.Rmd"))$ecoinvent_scores
   out <- format_robust_snapshot(ecoinvent_scores)
