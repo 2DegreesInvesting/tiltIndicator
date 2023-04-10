07bfea347b56be432af5ca983c8e51a18f0d8473
Mauro Lepore <maurolepore@gmail.com>
2023-04-10 18:36:12 +0000

    Characterize ictr_score_companies()

--- R/ictr_data.R
+++ R/ictr_data.R
@@ -10,26 +10,3 @@
 #' @examples
 #' ictr_companies
 "ictr_companies"
-
-#' Toy `ictr_inputs`
-#'
-#' @param input_co2,input_sector,unit See [ictr_inputs].
-#' @inheritDotParams tibble::tibble
-#'
-#' @family ICTR datasets
-#'
-#' @return A [data.frame].
-#' @export
-#'
-#' @examples
-#' ictr_toy_inputs()
-#' ictr_toy_inputs(unit = NULL)
-#' ictr_toy_inputs(new = 1)
-ictr_toy_inputs <- function(input_co2 = 1, input_sector = "a", unit = "u", ...) {
-  tibble::tibble(
-    input_co2 = input_co2,
-    input_sector = input_sector,
-    unit = unit,
-    ...
-  )
-}
