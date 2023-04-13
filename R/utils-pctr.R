113369035931420055d5359b87a007f21f9446b3
Mauro Lepore <maurolepore@gmail.com>
2023-04-13 00:12:01 +0000

    Explore rows

--- R/utils-pctr.R
+++ R/utils-pctr.R
@@ -16,14 +16,37 @@ pctr_toy_pctr_ecoinvent_co2 <- function(sec = "Transport",
   )
 }
 
-#' Crucial columns for pctr_score_companies
+pctr_ecoinvent_co2_crucial <- function() {
+  crucial_in_pctr_score_activities <- c(
+    "co2_footprint",
+    "sec",
+    "unit"
+  )
+
+  crucial_in_pctr_score_companies <- c(
+    "activity_product_uuid",
+    "ei_activity"
+  )
+
+  c(crucial_in_pctr_score_activities, crucial_in_pctr_score_companies)
+}
+
+pctr_companies_crucial <- function() {
+  c(
+    "activity_product_uuid",
+    "company_id",
+    "ei_activity",
+    "unit"
+  )
+}
+
 pctr_score_companies_crucial <- function() {
   c(
-    "unit",
-    "score_unit_sec",
-    "ei_activity",
     "activity_product_uuid",
+    "ei_activity",
     "score_all",
-    "score_unit"
+    "score_unit",
+    "score_unit_sec",
+    "unit"
   )
 }
--- tests/testthat/test-pctr_score_companies.R
+++ tests/testthat/test-pctr_score_companies.R
@@ -6,12 +6,53 @@ test_that("hasn't changed", {
   expect_snapshot(out)
 })
 
-test_that("with crucial columns throws no error", {
+test_that("with crucial columns in `pctr_ecoinvent_co2` throws no error", {
   data <- pctr_ecoinvent_co2 |>
     pctr_score_activities() |>
     select(all_of(pctr_score_companies_crucial()))
 
   expect_no_error(pctr_score_companies(data, pctr_companies))
+})
+
+test_that("with crucial columns in `pctr_companies` throws no error", {
+  data <- pctr_ecoinvent_co2 |>
+    pctr_score_activities() |>
+    select(all_of(pctr_score_companies_crucial()))
+
+  crucial_companies <- pctr_companies |>
+    select(all_of(pctr_companies_crucial()))
+  pctr_score_companies(data, crucial_companies) |>
+    expect_no_error()
+})
+
+test_that("without crucial columns in `pctr_companies` throws an error", {
+  data <- pctr_ecoinvent_co2 |>
+    pctr_score_activities() |>
+    select(all_of(pctr_score_companies_crucial()))
+
+  bad_companies <- pctr_companies |>
+    select(all_of(pctr_companies_crucial())) |>
+    select(-"company_id")
+  pctr_score_companies(data, bad_companies) |>
+    expect_error("company_id")
+
+  bad_companies <- pctr_companies |>
+    select(all_of(pctr_companies_crucial())) |>
+    select(-"activity_product_uuid")
+  pctr_score_companies(data, bad_companies) |>
+    expect_error("activity_product_uuid")
+
+  bad_companies <- pctr_companies |>
+    select(all_of(pctr_companies_crucial())) |>
+    select(-"ei_activity")
+  pctr_score_companies(data, bad_companies) |>
+    expect_error("ei_activity")
+
+  bad_companies <- pctr_companies |>
+    select(all_of(pctr_companies_crucial())) |>
+    select(-"unit")
+  pctr_score_companies(data, bad_companies) |>
+    expect_error("unit")
 })
 
 test_that("without crucial columns errors gracefully", {
