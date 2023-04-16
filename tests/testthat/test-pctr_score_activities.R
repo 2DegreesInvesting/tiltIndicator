3bffb77bda748514a2fd0472232a7df4eeb011c3
Mauro Lepore <maurolepore@gmail.com>
2023-04-16 16:01:48 +0000

    Refactor

--- tests/testthat/test-pctr_score_activities.R
+++ tests/testthat/test-pctr_score_activities.R
@@ -1,10 +1,3 @@
-test_that("hasn't change", {
-  out <- pctr_ecoinvent_co2 |>
-    pctr_score_activities(low_threshold = 0.3, high_threshold = 0.7) |>
-    format_robust_snapshot()
-  expect_snapshot(out)
-})
-
 test_that("with minimal data outptus a tibble", {
   data <- slice(pctr_ecoinvent_co2, 1)
   out <- pctr_score_activities(data)
