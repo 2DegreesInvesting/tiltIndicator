bfc27933beca622b46110d09b09333a0ccf82b2a
kalashsinghal <125359076+kalashsinghal@users.noreply.github.com>
2023-04-16 17:59:46 +0200

    Remove non-crucial columns ictr (#117)
    * remove ictr columns
    
    * Update snapshots
    
    ---------
    
    Co-authored-by: Mauro Lepore <maurolepore@gmail.com>

--- R/utils-ictr.R
+++ R/utils-ictr.R
@@ -46,3 +46,27 @@ ictr_toy_companies <- function() {
   )
   # styler: on
 }
+
+ictr_inputs_crucial <- function() {
+  crucial_in_ictr_score_activities <- c(
+    "input_co2",
+    "input_sector",
+    "unit"
+  )
+
+  crucial_in_ictr_score_companies <- c(
+    "activity_product_uuid",
+    "ei_activity"
+  )
+
+  c(crucial_in_ictr_score_activities, crucial_in_ictr_score_companies)
+}
+
+ictr_companies_crucial <- function() {
+  c(
+    "activity_product_uuid",
+    "company_id",
+    "ei_activity",
+    "unit"
+  )
+}
