d06bf91a3bbe39962bfab4e41705215134f1b036
delacombazlinda <delacombaz.linda@gmail.com>
2023-04-17 16:39:58 +0000

    changed Tilman's suggestions

--- R/pstr_data.R
+++ R/pstr_data.R
@@ -1,13 +1,13 @@
 #' A dataset with fake companies, products, sectors and subsectors
 #'
 #' @author Linda Delacombaz.
-#' @source Sectors, subsectors and products from Europages.
+#' @source Companies, sectors, subsectors and products from Europages.
 #' @family PSTR datasets
 #' @examples
 #' pstr_companies
 "pstr_companies"
 
-#' A bridge between sector and subsectors in europages and in `weo_2022`.
+#' A sector mapper between Europages and `weo_2022`.
 #'
 #' @author Linda Delacombaz.
 #' @source Internal source.
@@ -21,7 +21,7 @@
 #' * TODO: Where does it come from and what wrangling did you do?
 #'
 #' @author Linda Delacombaz.
-#' @source Data from IEA's WEO scenarios.
+#' @source Data from IEA's WEO 2022 scenario on GLO level.
 #' @family PSTR datasets
 #' @examples
 #' pstr_weo_2022
