5a795854bb6511eb34c1ba0021fec16f3ee15acd
Mauro Lepore <maurolepore@gmail.com>
2023-04-17 23:49:04 +0000

    Snapshot

--- tests/testthat/test-istr.R
+++ tests/testthat/test-istr.R
@@ -4,3 +4,11 @@ test_that("`reshaped_istr_companies` remains the same", {
   out <- format_robust_snapshot(reshaped_istr_companies)
   expect_snapshot(out)
 })
+
+test_that("`istr_companies` remains the same", {
+  path <- mvp_path("istr.Rmd")
+  istr_companies <- render_to_list(path)$istr_companies
+  out <- format_robust_snapshot(head(istr_companies))
+  expect_snapshot(out)
+})
+
