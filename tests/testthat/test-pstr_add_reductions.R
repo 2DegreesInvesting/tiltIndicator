22f912916520b226264033a523c344a809c3b5b7
Linda Delacombaz <97972818+lindadelacombaz@users.noreply.github.com>
2023-04-17 02:51:27 +0200

    Test `pstr_add_transition_risk()` and `pstr_add_reductions()` (#86)
    * created util file with the toy functions
    
    * added ... to add new variables to toy datasets
    
    * writing first tests for the add reductions functions
    
    * added purrr for map_chr function
    
    * added names of parameters
    
    * test for pstr_add_reductions done
    
    * noticed not wanted behaviour for columns names
    
    * styled version
    
    * fixed error thanks to test
    
    * added tests for add_transition_risk()
    
    * styled version
    
    * fixed error
    
    * Remove needless and brittle snapshot tests
    
    * Refactor (#132)
    
    * Refactor
    
    * style
    
    * Remove FIXME
    
    Following the principle of least surprise, I think it's best for
    pstr_add_reductions() to do just that and not deal with renaming.
    
    ---------
    
    Co-authored-by: Mauro Lepore <maurolepore@gmail.com>

--- tests/testthat/test-pstr_add_reductions.R
+++ tests/testthat/test-pstr_add_reductions.R
@@ -1,8 +1,82 @@
-test_that("hasn't changed", {
-  with_reductions <- pstr_companies |>
-    pstr_add_reductions(pstr_ep_weo, pstr_weo_2022) |>
-    # FIXME: Address warning
-    suppressWarnings()
-  out <- format_robust_snapshot(with_reductions)
-  expect_snapshot(out)
+test_that("with missing obligatory arguments errors gracefully", {
+  companies <- pstr_toy_companies()
+  ep_weo <- pstr_toy_ep_weo()
+  weo <- pstr_toy_weo_2022()
+
+  expect_error(pstr_add_reductions(companies, ep_weo = ep_weo), "weo_2022")
+  expect_error(pstr_add_reductions(companies, weo_2022 = weo_2022), "ep_weo")
+  expect_error(pstr_add_reductions(ep_weo = ep_weo, weo = weo), "companies")
+})
+
+test_that("returns a tibble", {
+  companies <- pstr_toy_companies()
+  ep_weo <- pstr_toy_ep_weo()
+  weo <- pstr_toy_weo_2022()
+
+  out <- pstr_add_reductions(companies, ep_weo, weo)
+
+  expect_s3_class(out, "tbl_df")
+})
+
+test_that("with pstr_toy_* companies outputs the expected columns", {
+  companies <- pstr_toy_companies()
+  ep_weo <- pstr_toy_ep_weo()
+  weo <- pstr_toy_weo_2022()
+
+  out <- pstr_add_reductions(companies, ep_weo, weo)
+
+  expected <- c(
+    "company_id",
+    "company_name",
+    "products",
+    "sector",
+    "subsector",
+    "EP_categories_id",
+    "EP_group",
+    "weo_product_mapper",
+    "weo_flow_mapper",
+    "publication",
+    "scenario",
+    "region",
+    "category",
+    "unit",
+    "year",
+    "value",
+    "reductions"
+  )
+  expect_named(out, expected)
+})
+
+test_that("additional columns appear in the output", {
+  companies <- pstr_toy_companies(x = 1)
+  ep_weo <- pstr_toy_ep_weo(y = 1)
+  weo <- pstr_toy_weo_2022(z = 1)
+
+  out <- pstr_add_reductions(companies, ep_weo, weo)
+  expect_true(hasName(out, "x"))
+  expect_true(hasName(out, "y"))
+  expect_true(hasName(out, "z"))
+})
+
+test_that("outputs columns of the same type as in `companies`", {
+  companies <- pstr_toy_companies()
+  ep_weo <- pstr_toy_ep_weo()
+  weo <- pstr_toy_weo_2022()
+
+  out <- pstr_add_reductions(companies, ep_weo, weo)
+
+  x <- unname(purrr::map_chr(companies, typeof))
+  y <- unname(purrr::map_chr(out[names(companies)], typeof))
+  expect_equal(x, y)
+})
+
+test_that("with 0-row companies outputs 0 rows", {
+  companies <- pstr_toy_companies()
+  ep_weo <- pstr_toy_ep_weo()
+  weo <- pstr_toy_weo_2022()
+  empty <- companies[0L, ]
+
+  out <- pstr_add_reductions(empty, ep_weo, weo)
+
+  expect_equal(nrow(out), 0L)
 })
