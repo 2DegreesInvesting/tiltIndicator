07d758c7fb8350afae628b445bafc274f7475e3b
Mauro Lepore <maurolepore@gmail.com>
2023-04-18 00:08:32 +0000

    New data istr_ep_weo

--- data-raw/istr_data.R
+++ data-raw/istr_data.R
@@ -2,5 +2,10 @@ library(readr)
 library(here)
 library(usethis)
 
-istr_companies <- read_csv(here("data-raw", "istr", "istr_companies.csv"), show_col_types = FALSE)
+options(readr.show_col_types = FALSE)
+
+istr_companies <- read_csv(here("data-raw", "istr", "istr_companies.csv"))
 use_data(istr_companies, overwrite = TRUE)
+
+istr_ep_weo <- read_csv(here("data-raw", "istr", "istr_ep_weo.csv"))
+use_data(istr_ep_weo, overwrite = TRUE)
