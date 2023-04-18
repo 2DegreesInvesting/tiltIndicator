53c1df939b11372ba3044002f2809c73a7045df2
Mauro Lepore <maurolepore@gmail.com>
2023-04-17 21:33:39 -0300

    Add ISTR (#144)
    * Add istr mvp
    
    * Use article
    
    * Document authorship with FIXME
    
    * Snapshot
    
    * Snapshot
    
    * Snapshot
    
    * Snapshot
    
    * Snapshot
    
    * Add data
    
    * Add data
    
    * Document
    
    * New data istr_ep_weo
    
    * Add data
    
    * Document
    
    * Use data
    
    * Add data
    
    * Refactor
    
    * Refactor
    
    * Update _pkgdown.yaml

---
+++ tests/testthat/test-istr.R
@@ -0,0 +1,35 @@
+test_that("`reshaped_istr_companies` remains the same", {
+  path <- mvp_path("istr.Rmd")
+  reshaped_istr_companies <- render_to_list(path)$reshaped_istr_companies
+  out <- format_robust_snapshot(reshaped_istr_companies)
+  expect_snapshot(out)
+})
+
+test_that("`istr_companies` remains the same", {
+  path <- mvp_path("istr.Rmd")
+  istr_companies <- render_to_list(path)$istr_companies
+  out <- format_robust_snapshot(head(istr_companies))
+  expect_snapshot(out)
+})
+
+test_that("`with_reductions` remains the same", {
+  path <- mvp_path("istr.Rmd")
+  with_reductions <- render_to_list(path)$with_reductions
+  out <- format_robust_snapshot(head(with_reductions))
+  expect_snapshot(out)
+})
+
+test_that("`with_transition_risk` remains the same", {
+  path <- mvp_path("istr.Rmd")
+  with_transition_risk <- render_to_list(path)$with_transition_risk
+  out <- format_robust_snapshot(head(with_transition_risk))
+  expect_snapshot(out)
+})
+
+test_that("`with_score_aggregated` remains the same", {
+  path <- mvp_path("istr.Rmd")
+  with_score_aggregated <- render_to_list(path)$with_score_aggregated
+  out <- format_robust_snapshot(with_score_aggregated)
+  expect_snapshot(out)
+})
+
