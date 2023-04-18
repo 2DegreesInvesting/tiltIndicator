78bd3c9b38419bf8f12ac251f051f7c38b9b37aa
Mauro Lepore <maurolepore@gmail.com>
2023-04-18 00:13:05 +0000

    Use data

--- data-raw/istr_data.R
+++ data-raw/istr_data.R
@@ -9,3 +9,6 @@ use_data(istr_companies, overwrite = TRUE)
 
 istr_ep_weo <- read_csv(here("data-raw", "istr", "istr_ep_weo.csv"))
 use_data(istr_ep_weo, overwrite = TRUE)
+
+istr_weo_2022 <- read_csv(here("data-raw", "istr", "istr_weo_2022.csv"))
+use_data(istr_weo_2022, overwrite = TRUE)
