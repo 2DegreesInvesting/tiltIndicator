76e99626dc07b5c3070a0f8b43534a5f939f22c1
Mauro Lepore <maurolepore@gmail.com>
2023-04-19 18:49:09 +0000

    Move istr_mapping() to R/

---
+++ R/istr_mapping.R
@@ -0,0 +1,24 @@
+#' Join the Ecoinvent's' sectors and the WEO 2022 file on the sectors by use the mapper that serves as a sector classification bridge between.
+#'
+#'
+#' @param companies A [data.frame] like [istr_companies].
+#' @param ep_weo A [data.frame] like [istr_ep_weo].
+#' @param weo_2022 A [data.frame] like [istr_weo_2022].
+#'
+#' @family ISTR functions
+#'
+#' @return A dataframe with:
+#'   * All the columns from the `companies` dataset.
+#'   * New columns:
+#'       * All the columns from the `ep_weo` dataset, but the columns
+#'       `eco_sectors` is named `ECO_sector` respectively.
+#'
+#' @export
+#'
+#' @examples
+#' istr_companies |>
+#'   istr_add_reductions(istr_ep_weo, istr_weo_2022)
+istr_mapping <- function(companies, ep_weo) {
+  companies |>
+    left_join(ep_weo, by = c("eco_sectors" = "ECO_sector"))
+}
