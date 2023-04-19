f90805241ee645b214172d82d3726bf3a4326a9f
Mauro Lepore <maurolepore@gmail.com>
2023-04-19 16:03:47 +0000

    FIXME: Make it pipable

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -2,3 +2,8 @@ test_that("stop_if_any_missing_input_co2 throws the expected error", {
   data <- tibble(input_co2 = NA)
   expect_error(stop_if_any_missing_input_co2(data), "input_co2.*missing")
 })
+
+test_that("stop_if_any_missing_input_co2 is pipable", {
+  data <- tibble(input_co2 = 1)
+  expect_equal(stop_if_any_missing_input_co2(data), data)
+})
