2386660ccbd2c0efce77bdd12ea65bd4a074898a
delacombazlinda <delacombaz.linda@gmail.com>
2023-04-19 09:41:49 +0000

    added test for aggregate_scores.R

--- tests/testthat/test-pstr_aggregate_scores.R
+++ tests/testthat/test-pstr_aggregate_scores.R
@@ -7,3 +7,16 @@ test_that("FIXME? Do we intend to return a grouped data frame?", {
 
   expect_true(dplyr::is_grouped_df(out))
 })
+
+test_that("outputs a result for companies without any products", {
+  companies <- pstr_toy_companies('products' = NA)
+  ep_weo <- pstr_toy_ep_weo()
+  weo <- pstr_toy_weo_2022()
+
+  out <- companies |>
+    pstr_add_reductions(ep_weo, weo) |>
+    pstr_add_transition_risk() |>
+    pstr_aggregate_scores(companies)
+
+  expect_equal(FALSE, is.null(out$score_aggregated))
+})
