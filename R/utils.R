87acd6f2a376615d094fb45196f8f1d93681ddcc
Mauro Lepore <maurolepore@gmail.com>
2023-04-19 15:42:58 +0000

    Move missing_co2 to R/utils.R
    This function seems useful for developer but users should likely not
    interact with it directy. It should be called not by a user but by
    another function that wants to assert that the data has no missing value.
    
    Thus the function should not be exported and also not tested directy but
    instead through the higher level, exported function that depends on it.
    
    I'll follow that up in another commit.

--- R/utils.R
+++ R/utils.R
@@ -47,3 +47,25 @@ use_refactoring_checklist <- function() {
   - [ ] Update CODEOWNWERS.
   ")
 }
+
+#' Throw error for NA values in co2 column
+#'
+#' @description
+#' Stop the workflow with an error message if data is missing in co2 column. We
+#' can replace/delete the NA values later through this function if needed. For
+#' now, it stops the workflow with an error message for user to act upon it.
+#'
+#' @param data A dataframe/tibble
+#'
+#' @export
+#'
+#' @examples
+#' library(tibble, warn.conflicts = FALSE)
+#'
+#' data <- tibble::tibble(input_co2 = c(1, 4))
+#' missing_co2(data)
+missing_co2 <- function(data) {
+  if (anyNA(data$input_co2)) {
+    stop("Each `input_co2` must not be missing.")
+  }
+}
