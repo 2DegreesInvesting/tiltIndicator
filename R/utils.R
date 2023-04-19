f0a20cc875e66f7b5d76ebdd87318d44e0ff1616
Mauro Lepore <maurolepore@gmail.com>
2023-04-19 15:48:39 +0000

    missing_co2() is internal

--- R/utils.R
+++ R/utils.R
@@ -48,22 +48,16 @@ use_refactoring_checklist <- function() {
   ")
 }
 
-#' Throw error for NA values in co2 column
-#'
-#' @description
-#' Stop the workflow with an error message if data is missing in co2 column. We
-#' can replace/delete the NA values later through this function if needed. For
-#' now, it stops the workflow with an error message for user to act upon it.
+#' Throws an error if `data` has NA values in the co2 column
 #'
 #' @param data A dataframe/tibble
-#'
-#' @export
 #'
 #' @examples
 #' library(tibble, warn.conflicts = FALSE)
 #'
 #' data <- tibble::tibble(input_co2 = c(1, 4))
 #' missing_co2(data)
+#' @noRd
 missing_co2 <- function(data) {
   if (anyNA(data$input_co2)) {
     stop("Each `input_co2` must not be missing.")
