4bffa0f2cae99aa1dfa859c36be2485d49832141
Mauro Lepore <maurolepore@gmail.com>
2023-04-19 18:52:49 +0000

    Move more functions to R/

---
+++ R/istr_add_reductions.R
@@ -0,0 +1,29 @@
+#' Add the emission reduction targets to the companies dataset
+#'
+#' Adds the emission reduction values for each company's product(s).
+#'
+#' @param companies A [data.frame] like [istr_companies].
+#' @param ep_weo A [data.frame] like [istr_ep_weo].
+#' @param weo_2022 A [data.frame] like [istr_weo_2022].
+#'
+#' @family ISTR functions
+#'
+#' @return A dataframe with:
+#'   * All the columns from the `companies` dataset.
+#'   * New columns:
+#'       * All the columns from the `ep_weo` dataset, but the columns
+#'       `eco_sectors` is named `ECO_sector` respectively.
+#'       * All the columns from the `weo_2022` dataset, but the columns
+#'       `weo_product_mapper` and `weo_flow_mapper` are named `product` and
+#'       `flow`, respectively.
+#'
+#' @export
+#'
+#' @examples
+#' istr_companies |>
+#'   istr_add_reductions(istr_ep_weo, istr_weo_2022)
+istr_add_reductions <- function(companies, weo_2022) {
+  companies |>
+    #left_join(ep_weo, by = c("eco_sectors" = "ECO_sector")) |>
+    left_join(weo_2022, by = c("weo_product_mapper" = "product", "weo_flow_mapper" = "flow"))
+}
---
+++ R/istr_add_transition_risk.R
@@ -0,0 +1,39 @@
+#' Categorize sector emission reduction targets
+#'
+#' Translates the `reductions` column into three categories:
+#' * "low" if `reductions` <= 30.0
+#' * "medium" if 30.0 < `reductions` <= 70.0
+#' * "high" if `reductions` > 70.0
+#'
+#' @param with_reductions A data frame. The output of [istr_add_reductions()].
+#'
+#' @family ISTR functions
+#'
+#' @return A data frame with:
+#'    * All the columns from the `companies` dataset.
+#'    * All the columns from the `ep_weo` dataset, but the columns
+#'    `eco_sectors` is named `ECO_sector` respectively.
+#'    * All the columns from the `weo_2022` dataset, but the columns
+#'    `weo_product_mapper` and `weo_flow_mapper` are named `product` and
+#'    `flow`, respectively.
+#'    * New column:
+#'       * `transition_risk` that holds the categorization of the `reductions`
+#'       column.
+#'
+#' @export
+#'
+#' @examples
+#' istr_companies |>
+#'   istr_add_reductions(istr_ep_weo, istr_weo_2022) |>
+#'   istr_add_transition_risk()
+istr_add_transition_risk <- function(with_reductions) {
+  with_reductions |>
+    mutate(
+      transition_risk = case_when(
+        reductions <= 30 ~ "low",
+        reductions > 30 & reductions <= 70 ~ "medium",
+        reductions >= 70 ~ "high",
+        TRUE ~ "no_sector",
+      )
+    )
+}
