d3b0843275ce48631598f3d9909750a13dc2245d
Mauro Lepore <maurolepore@gmail.com>
2023-04-20 21:13:10 +0000

    Duplicate R and test files

---
+++ R/pstr_add_reductions_old.R
@@ -0,0 +1,28 @@
+#' Add the emission reduction targets to the companies dataset
+#'
+#' Adds the emission reduction values for each company's product(s).
+#'
+#' @author Linda Delacombaz.
+#'
+#' @param companies A [data.frame] like [pstr_companies].
+#' @param ep_weo A [data.frame] like [pstr_ep_weo].
+#' @param weo_2022 A [data.frame] like [pstr_weo_2022].
+#'
+#' @family PSTR functions
+#'
+#' @return A dataframe with:
+#'   * All the columns from the `companies` dataset.
+#'   * New columns:
+#'       * All the columns from the `ep_weo` dataset, but the columns
+#'       `EP_sector` and `EP_subsector` are named `sector` and `subsector`
+#'       respectively.
+#' @export
+#'
+#' @examples
+#' pstr_companies |>
+#'   pstr_add_reductions_old(pstr_ep_weo, pstr_weo_2022)
+pstr_add_reductions_old <- function(companies, ep_weo, weo_2022) {
+  companies |>
+    left_join(ep_weo, by = c("sector" = "EP_sector", "subsector" = "EP_subsector")) |>
+    left_join(weo_2022, by = c("weo_product_mapper" = "product", "weo_flow_mapper" = "flow"))
+}
---
+++ tests/testthat/test-pstr_add_reductions_old.R
@@ -0,0 +1,94 @@
+test_that("(old) with missing obligatory arguments errors gracefully", {
+  companies <- pstr_toy_companies()
+  ep_weo <- pstr_toy_ep_weo()
+  weo <- pstr_toy_weo_2022()
+
+  expect_error(pstr_add_reductions_old(companies, ep_weo = ep_weo), "weo_2022")
+  expect_error(pstr_add_reductions_old(companies, weo_2022 = weo_2022), "ep_weo")
+  expect_error(pstr_add_reductions_old(ep_weo = ep_weo, weo = weo), "companies")
+})
+
+test_that("(old) returns a tibble", {
+  companies <- pstr_toy_companies()
+  ep_weo <- pstr_toy_ep_weo()
+  weo <- pstr_toy_weo_2022()
+
+  out <- pstr_add_reductions_old(companies, ep_weo, weo)
+
+  expect_s3_class(out, "tbl_df")
+})
+
+test_that("(old) with pstr_toy_* companies outputs the expected columns", {
+  companies <- pstr_toy_companies()
+  ep_weo <- pstr_toy_ep_weo()
+  weo <- pstr_toy_weo_2022()
+
+  out <- pstr_add_reductions_old(companies, ep_weo, weo)
+
+  expected <- c(
+    "company_id",
+    "company_name",
+    "products",
+    "sector",
+    "subsector",
+    "EP_categories_id",
+    "EP_group",
+    "weo_product_mapper",
+    "weo_flow_mapper",
+    "publication",
+    "scenario",
+    "region",
+    "category",
+    "unit",
+    "year",
+    "value",
+    "reductions"
+  )
+  expect_named(out, expected)
+})
+
+test_that("(old) additional columns appear in the output", {
+  companies <- pstr_toy_companies(x = 1)
+  ep_weo <- pstr_toy_ep_weo(y = 1)
+  weo <- pstr_toy_weo_2022(z = 1)
+
+  out <- pstr_add_reductions_old(companies, ep_weo, weo)
+  expect_true(hasName(out, "x"))
+  expect_true(hasName(out, "y"))
+  expect_true(hasName(out, "z"))
+})
+
+test_that("(old) outputs columns of the same type as in `companies`", {
+  companies <- pstr_toy_companies()
+  ep_weo <- pstr_toy_ep_weo()
+  weo <- pstr_toy_weo_2022()
+
+  out <- pstr_add_reductions_old(companies, ep_weo, weo)
+
+  x <- unname(purrr::map_chr(companies, typeof))
+  y <- unname(purrr::map_chr(out[names(companies)], typeof))
+  expect_equal(x, y)
+})
+
+test_that("(old) with 0-row companies outputs 0 rows", {
+  companies <- pstr_toy_companies()
+  ep_weo <- pstr_toy_ep_weo()
+  weo <- pstr_toy_weo_2022()
+  empty <- companies[0L, ]
+
+  out <- pstr_add_reductions_old(empty, ep_weo, weo)
+
+  expect_equal(nrow(out), 0L)
+})
+
+test_that("(old) still adds reductions without any products", {
+  companies <- pstr_toy_companies()
+  ep_weo <- pstr_toy_ep_weo()
+  weo <- pstr_toy_weo_2022()
+  empty_products <- pstr_toy_companies("products" = NA)
+
+  out <- pstr_add_reductions_old(companies, ep_weo, weo)
+  out_1 <- pstr_add_reductions_old(empty_products, ep_weo, weo)
+
+  expect_equal(out$reductions, out_1$reductions)
+})
