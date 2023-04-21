3405f759f9a7e2285b1a49e6596ccc40b98b171b
Kalash Singhal <kalash@2degrees-investing.org>
2023-04-21 09:27:22 +0200

    deleted old data

--- data-raw/ictr_data.R
+++ data-raw/ictr_data.R
@@ -5,10 +5,10 @@ library(dplyr)
 devtools::load_all()
 
 # The raw data comes from tiltIndicator#87, which includes pre-processing
-ictr_inputs <- vroom(here("data-raw", "ictr", "real_data", "ictr_inputs.csv"), show_col_types = FALSE) |>
+ictr_inputs <- vroom(here("data-raw", "ictr", "ictr_inputs.csv"), show_col_types = FALSE) |>
   select(all_of(ictr_inputs_crucial()))
 use_data(ictr_inputs, overwrite = TRUE)
 
-ictr_companies <- vroom(here("data-raw", "ictr", "real_data", "ictr_companies.csv"), show_col_types = FALSE) |>
+ictr_companies <- vroom(here("data-raw", "ictr", "ictr_companies.csv"), show_col_types = FALSE) |>
   select(all_of(ictr_companies_crucial()))
 use_data(ictr_companies, overwrite = TRUE)
