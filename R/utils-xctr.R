0c40f6cb8ca270f8b9ff0023d0e2027b555c66d9
Mauro Lepore <maurolepore@gmail.com>
2023-04-24 21:23:45 +0000

    rcmd

--- R/utils-xctr.R
+++ R/utils-xctr.R
@@ -1,5 +1,5 @@
 xctr_rename <- function(data) {
   data |>
-    rename(id = company_id, transition_risk = score) |>
+    rename(id = "company_id", transition_risk = "score") |>
     rename_with(~ gsub("share_", "score_", .x), starts_with("share_"))
 }
