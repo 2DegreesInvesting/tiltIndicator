23b2a565cf06430fb6f3a5afe223bb691cd4a715
Mauro Lepore <maurolepore@gmail.com>
2023-04-24 15:49:28 +0000

    Toy data

---
+++ R/utils-istr.R
@@ -0,0 +1,23 @@
+istr_toy_companies <- function() {
+  tibble(
+    company_name = "abc",
+    eco_sectors = "steel_metal_transformation")
+}
+
+istr_toy_ep_weo <- function() {
+  tibble(
+    ECO_sector = "steel_metal_transformation",
+    weo_product_mapper = "Total",
+    weo_flow_mapper = "Iron and steel"
+  )
+}
+
+istr_toy_weo <- function() {
+  tibble(
+    scenario = "Stated Policies Scenario",
+    product = "Total",
+    flow = "Road passenger light duty vehicle",
+    year = 2020,
+    reductions = 0
+  )
+}
