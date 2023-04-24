d5a92b8d466da16d130b962c5a625570a872e934
Mauro Lepore <maurolepore@gmail.com>
2023-04-24 22:18:46 +0000

    style

--- R/ictr.R
+++ R/ictr.R
@@ -17,9 +17,9 @@
 ictr <- function(companies, inputs, low_threshold = 0.3, high_threshold = 0.7) {
   out <- inputs |>
     ictr_score_inputs(
-    low_threshold = low_threshold,
-    high_threshold = high_threshold
-  ) |>
+      low_threshold = low_threshold,
+      high_threshold = high_threshold
+    ) |>
     ictr_score_companies(companies)
 
   out |>
