a0087dcb0e072042a213472ad458cd5ab6053a79
Mauro Lepore <maurolepore@gmail.com>
2023-04-24 21:32:52 +0000

    Refactor

--- R/pstr.R
+++ R/pstr.R
@@ -16,8 +16,8 @@
 #' @examples
 #' pstr(pstr_companies, pstr_weo_2022, pstr_ep_weo)
 pstr <- function(companies, scenario, mapper = NULL) {
-  companies |>
-    pstr_add_reductions(mapper, scenario) |>
+  with_reductions <- pstr_add_reductions(companies, mapper, scenario)
+  with_reductions |>
     pstr_add_transition_risk() |>
     pstr_aggregate_scores(companies) |>
     rename(id = "company_id")
