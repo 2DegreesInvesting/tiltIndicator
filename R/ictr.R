f84688d2eb720dd0a99326bfda2eea9f10adb50c
Mauro Lepore <maurolepore@gmail.com>
2023-04-24 20:49:21 +0000

    wip

--- R/ictr.R
+++ R/ictr.R
@@ -16,15 +16,6 @@
 #' @examples
 #' ictr(ictr_companies, ictr_inputs)
 ictr <- function(companies, inputs) {
-  rename_xctr <- function(data) {
-    data |>
-      dplyr::rename(
-        companies_id = company_id,
-        transition_risk = score
-      ) |>
-      dplyr::rename_with(~gsub("share_", "score_", .x), dplyr::starts_with("share_"))
-  }
-
   out <- ictr_score_companies(ictr_score_inputs(inputs), companies)
   rename_xctr(out)
 }
