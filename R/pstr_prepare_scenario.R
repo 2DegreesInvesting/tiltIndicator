25fcbf08e1b9ec02adb114ee945fe0cf210b833f
delacombazlinda <delacombaz.linda@gmail.com>
2023-04-25 08:42:31 +0000

    added new prepare scenario function

---
+++ R/pstr_prepare_scenario.R
@@ -0,0 +1,37 @@
+#' Prepare the scenario data set
+#'
+#' @param ipr A [data.frame] like [pstr_ipr_2022].
+#' @param weo A [data.frame] like [pstr_weo_2022].
+#'
+#' @return A dataframe with:
+#'   * All the columns from the `ipr` dataset.
+#'   * New columns:
+#'       * All the columns from the `weo` dataset.
+#' @export
+#'
+#' @examples
+pstr_prepare_scenario <- function(ipr, weo) {
+  bind_rows(
+    pstr_prepare_scenario_impl(ipr, "ipr"),
+    pstr_prepare_scenario_impl(weo, "weo")
+  )
+}
+#' Prepare the scenario
+#'
+#' TODO : document the function
+#'
+#' @param data
+#' @param type
+#'
+#' @return
+#'
+#' @export
+#'
+#' @examples
+pstr_prepare_scenario_impl <- function(data, type) {
+  data |>
+    lowercase_characters() |>
+    rename_with(~ gsub(paste0(type, "_"), "", .x)) |>
+    mutate(type = type) |>
+    rename(reductions = co2_reductions)
+}
