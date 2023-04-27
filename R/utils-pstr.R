1fdc304ae475960b2053233731bfd0c3a118b696
Mauro Lepore <maurolepore@gmail.com>
2023-04-27 23:26:52 +0000

    refactor

--- R/utils-pstr.R
+++
@@ -1,7 +0,0 @@
-pstr_new_dataset_companies <- function() {
-  pstr_prepare_companies(pstr_raw_companies)
-}
-
-pstr_new_dataset_scenarios <- function() {
-  pstr_prepare_scenario(list(weo = pstr_raw_weo_2022, ipr = pstr_raw_ipr_2022))
-}
