2e6aca9223841b9221da16513b950740e58e4cf0
Mauro Lepore <maurolepore@gmail.com>
2023-04-27 22:35:49 +0000

    Prune

--- tests/testthat/test-pstr_new_add_reductions.R
+++
@@ -1,5 +0,0 @@
-test_that("works", {
-  scenarios <- pstr_new_dataset_scenarios()
-  companies <- pstr_new_dataset_companies() |> slice(1)
-  expect_s3_class(pstr_new_add_reductions(companies, scenarios), "tbl")
-})
