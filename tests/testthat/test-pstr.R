7f3f22c54979b2e2e6a31bbc5c2e86919dea2c54
Mauro Lepore <maurolepore@gmail.com>
2023-04-27 22:20:28 +0000

    refactor

--- tests/testthat/test-pstr.R
+++ tests/testthat/test-pstr.R
@@ -1,18 +1,6 @@
-test_that("the output is not grouped", {
-  weo <- slice(pstr_new_weo_2022, 1)
-  ipr <- slice(pstr_new_ipr_2022, 1)
-  scenarios <- pstr_prepare_scenario(list(weo = weo, ipr = ipr))
-  companies <- pstr_prepare_companies(slice(pstr_new_companies, 1))
-
-  out <- pstr(companies, scenarios)
-  expect_snapshot(format_robust_snapshot(out))
-})
-
 test_that("outputs the expected columns", {
-  weo <- slice(pstr_new_weo_2022, 1)
-  ipr <- slice(pstr_new_ipr_2022, 1)
-  scenarios <- pstr_prepare_scenario(list(weo = weo, ipr = ipr))
-  companies <- pstr_prepare_companies(slice(pstr_new_companies, 1))
+  scenarios <- pstr_new_dataset_scenarios()
+  companies <- pstr_new_dataset_companies()
 
   out <- pstr(companies, scenarios)
 
@@ -22,10 +10,8 @@ test_that("outputs the expected columns", {
 })
 
 test_that("the output is not grouped", {
-  weo <- slice(pstr_new_weo_2022, 1)
-  ipr <- slice(pstr_new_ipr_2022, 1)
-  scenarios <- pstr_prepare_scenario(list(weo = weo, ipr = ipr))
-  companies <- pstr_prepare_companies(slice(pstr_new_companies, 1))
+  scenarios <- pstr_new_dataset_scenarios()
+  companies <- pstr_new_dataset_companies()
 
   out <- pstr(companies, scenarios)
   expect_false(dplyr::is_grouped_df(out))
