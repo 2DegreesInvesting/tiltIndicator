2834c595af6f78264e125a17e7a00a9dd0607622
Mauro Lepore <maurolepore@gmail.com>
2023-04-27 21:59:39 +0000

    Rename

--- tests/testthat/test-pstr_data.R
+++ tests/testthat/test-pstr_data.R
@@ -1,11 +1,11 @@
-test_that("`pstr_companies` hasn't changed", {
-  expect_snapshot(format_robust_snapshot(pstr_companies))
+test_that("`pstr_new_companies` hasn't changed", {
+  expect_snapshot(format_robust_snapshot(pstr_new_companies))
 })
 
-test_that("`pstr_ep_weo` hasn't changed", {
-  expect_snapshot(format_robust_snapshot(pstr_ep_weo))
+test_that("`pstr_new_weo_2022` hasn't changed", {
+  expect_snapshot(format_robust_snapshot(pstr_new_weo_2022))
 })
 
-test_that("`pstr_weo_2022` hasn't changed", {
-  expect_snapshot(format_robust_snapshot(pstr_weo_2022))
+test_that("`pstr_new_ipr_2022` hasn't changed", {
+  expect_snapshot(format_robust_snapshot(pstr_new_weo_2022))
 })
--- tests/testthat/test-pstr_new_add_reductions.R
+++
@@ -1,8 +0,0 @@
-test_that("works", {
-  weo <- slice(pstr_new_weo_2022, 1)
-  ipr <- slice(pstr_new_ipr_2022, 1)
-  scenarios <- pstr_prepare_scenario(list(weo = weo, ipr = ipr))
-  companies <- pstr_prepare_companies(slice(pstr_new_companies, 1))
-  companies <- pstr_prepare_companies(pstr_new_companies)
-  expect_s3_class(pstr_new_add_reductions(companies, scenarios), "tbl")
-})
