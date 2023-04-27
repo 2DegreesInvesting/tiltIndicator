b197a8fbfc4a07b38f86382f1b4f7a8c20901754
Mauro Lepore <maurolepore@gmail.com>
2023-04-27 10:53:05 +0000

    Extract in place

--- R/ictr_score_companies.R
+++ R/ictr_score_companies.R
@@ -45,7 +45,22 @@ ictr_score_companies <- function(ecoinvent_scores, companies) {
   companies_scores <- companies |>
     left_join(ecoinvent_scores, by = c("activity_uuid_product_uuid"))
 
+  score <- function(data, suffix) {
+    score_col <- paste0("score_", suffix)
+    stopifnot(hasName(data, score_col))
+    share_col <- paste0("share_", suffix)
+
+    data |>
+      group_by(.data$company_id, .data[[score_col]]) |>
+      filter(!is.na(.data[[score_col]])) |>
+      summarise(.n = n()) |>
+      mutate({{share_col}} := .data$.n / sum(.data$.n)) |>
+      rename("score" = all_of(score_col)) |>
+      select(-all_of(".n"))
+  }
+
   ## scores in comparison to all input products
+  scores_all <- score(companies_scores, "all")
   scores_all <- companies_scores |>
     group_by(.data$company_id, .data$score_all) |>
     filter(!is.na(.data$score_all)) |>
