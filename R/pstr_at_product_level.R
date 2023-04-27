9fad98c6cebc76f83056d1999a81963bcc3efbd1
Mauro Lepore <maurolepore@gmail.com>
2023-04-27 23:41:52 +0000

    refactor

--- R/pstr_at_product_level.R
+++ R/pstr_at_product_level.R
@@ -29,3 +29,21 @@ pstr_add_reductions <- function(companies, scenarios) {
   )
 }
 
+#' Categorize sector emission reduction targets
+#'
+#' @examples
+#' pstr_old_companies |>
+#'   pstr_old_add_reductions(pstr_ep_weo, pstr_weo_2022) |>
+#'   pstr_add_transition_risk()
+#' @noRd
+pstr_add_transition_risk <- function(with_reductions) {
+  with_reductions |>
+    mutate(
+      transition_risk = case_when(
+        reductions <= 30 ~ "low",
+        reductions > 30 & reductions <= 70 ~ "medium",
+        reductions > 70 ~ "high",
+        TRUE ~ "no_sector",
+      )
+    )
+}
