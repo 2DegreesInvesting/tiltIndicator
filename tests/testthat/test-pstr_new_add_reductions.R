fd184878f7c2235a419da212d2306b002d286b3c
Mauro Lepore <maurolepore@gmail.com>
2023-04-27 19:50:27 +0000

    test

--- tests/testthat/test-pstr_new_add_reductions.R
+++ tests/testthat/test-pstr_new_add_reductions.R
@@ -3,5 +3,6 @@ test_that("works", {
   ipr <- slice(pstr_new_ipr_2022, 1)
   scenarios <- pstr_prepare_scenario(list(weo = weo, ipr = ipr))
   companies <- pstr_prepare_companies(slice(pstr_new_companies, 1))
+  companies <- pstr_prepare_companies(pstr_new_companies)
   expect_s3_class(pstr_new_add_reductions(companies, scenarios), "tbl")
 })
