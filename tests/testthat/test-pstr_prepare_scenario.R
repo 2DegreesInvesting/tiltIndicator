e4f84e6f1c761e701696f1492b9054b26b297057
Mauro Lepore <maurolepore@gmail.com>
2023-04-27 18:50:50 +0000

    wip

--- tests/testthat/test-pstr_prepare_scenario.R
+++ tests/testthat/test-pstr_prepare_scenario.R
@@ -4,3 +4,9 @@ test_that("outputs the expected snapshot", {
   out <- pstr_prepare_scenario(weo, ipr)
   expect_snapshot(format_robust_snapshot(out))
 })
+
+test_that("with weo twice type is only weo", {
+  weo <- slice(pstr_new_weo_2022, 1:3)
+  out <- pstr_prepare_scenario(weo, weo)
+  expect_equal(unique(out$type), "weo")
+})
