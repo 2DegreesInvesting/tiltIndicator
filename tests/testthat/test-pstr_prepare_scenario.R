a772eaa5099cb92d3cca6b2cd76bd56158dd5cd7
Mauro Lepore <maurolepore@gmail.com>
2023-04-27 19:17:11 +0000

    Vectorize

--- tests/testthat/test-pstr_prepare_scenario.R
+++ tests/testthat/test-pstr_prepare_scenario.R
@@ -8,7 +8,6 @@ test_that("outputs the expected snapshot", {
 })
 
 test_that("with weo twice type is only weo", {
-  skip("TODO")
   weo <- slice(pstr_new_weo_2022, 1:3)
   scenarios <- list(weo = weo, weo = weo)
   out <- pstr_prepare_scenario(scenarios)
