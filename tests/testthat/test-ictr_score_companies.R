cd595355e09e24768a333a22c7a37aaf5a8caa0f
Mauro Lepore <maurolepore@gmail.com>
2023-04-27 10:52:59 +0000

    Snap

--- tests/testthat/test-ictr_score_companies.R
+++ tests/testthat/test-ictr_score_companies.R
@@ -1,6 +1,13 @@
 test_that("hasn't change", {
   out <- ictr_inputs |>
     ictr_score_inputs() |>
+    ictr_score_companies(ictr_companies |> slice(1))
+  expect_snapshot(out)
+})
+
+test_that("hasn't change", {
+  out <- ictr_inputs |>
+    ictr_score_inputs() |>
     ictr_score_companies(ictr_companies) |>
     format_robust_snapshot()
   expect_snapshot(out)
