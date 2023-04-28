52b13ba8d14aee3ac72703133004def96d9d4d47
Mauro Lepore <maurolepore@gmail.com>
2023-04-28 23:09:44 +0000

    Refactor tests

--- tests/testthat/test-ictr.R
+++ tests/testthat/test-ictr.R
@@ -1,10 +1,12 @@
-test_that("outputs the expected columns", {
+test_that("hasn't change", {
+  out <- format_robust_snapshot(ictr(ictr_companies, ictr_inputs))
+  expect_snapshot(out)
+})
+
+test_that("outputs columns for ids, risk categories, and shares", {
   companies <- slice(ictr_companies, 1)
   inputs <- slice(ictr_inputs, 1)
   out <- ictr(companies, inputs)
-
-  expect_true(all(common_output_columns() %in% names(out)))
-  expect_true(any(grepl("score", names(out))))
 
   expected <- c(
     "id",
@@ -17,9 +19,176 @@ test_that("outputs the expected columns", {
   expect_equal(names(out)[1:6], expected)
 })
 
-test_that("outputs the expected columns", {
+test_that("returns 3 rows per company, for risk 'low', 'medium', and 'high'", {
+  inputs <- tibble(
+    activity_uuid_product_uuid = "x",
+    input_co2 = 1,
+    input_sector = "transport",
+    unit = "metric ton*km",
+  )
+
+  companies <- tibble(
+    company_id = "a",
+    activity_uuid_product_uuid = c("x", "y")
+  )
+  out <- ictr(companies, inputs)
+  expect_equal(nrow(out), 3L)
+  expect_equal(sort(unique(out$transition_risk)), c("high", "low", "medium"))
+
+  companies <- tibble(
+    company_id = c("a", "b"),
+    activity_uuid_product_uuid = c("x", "y")
+  )
+  out <- ictr(companies, inputs)
+  expect_equal(nrow(out), 6L)
+  expect_equal(sort(unique(out$transition_risk)), c("high", "low", "medium"))
+})
+
+test_that("if a company matches at least one input, each share sums 1 (#175)", {
+  companies <- tibble(
+    company_id = "a",
+    activity_uuid_product_uuid = c("a", "b")
+  )
+  inputs <- tibble(
+    activity_uuid_product_uuid = c("a"),
+    input_co2 = 1,
+    input_sector = "transport",
+    unit = "metric ton*km",
+  )
+
+  out <- ictr(companies, inputs)
+  sum_of_each_share <- unique(sapply(select(out, starts_with("score")), sum))
+  expect_equal(sum_of_each_share, 1)
+})
+
+test_that("if a company matches no inputs, all shares are `NA` (#176)", {
+  companies <- tibble(
+    company_id = "a",
+    activity_uuid_product_uuid = "a"
+  )
+  inputs <- tibble(
+    activity_uuid_product_uuid = "b",
+    input_co2 = 1,
+    input_sector = "transport",
+    unit = "metric ton*km",
+  )
+
+  out <- ictr(companies, inputs)
+
+  share_is_na <- is.na(unlist(select(out, starts_with("score"))))
+  expect_true(all(share_is_na))
+})
+
+test_that("if a company matches no inputs, all shares are `NA` (#176)", {
+  companies <- tibble(
+    company_id = c("a"),
+    activity_uuid_product_uuid = c("a")
+  )
+  inputs <- tibble(
+    activity_uuid_product_uuid = "b",
+    input_co2 = 1,
+    input_sector = "transport",
+    unit = "metric ton*km",
+  )
+
+  out <- ictr(companies, inputs)
+
+  share_is_na <- is.na(unlist(select(out, starts_with("score"))))
+  expect_true(all(share_is_na))
+})
+
+test_that("if a company matches at least one input, no share is `NA` (#176)", {
+  companies <- tibble(
+    company_id = "a",
+    activity_uuid_product_uuid = c("a", "b")
+  )
+  inputs <- tibble(
+    activity_uuid_product_uuid = c("a"),
+    input_co2 = 1,
+    input_sector = "transport",
+    unit = "metric ton*km",
+  )
+
+  out <- ictr(companies, inputs)
+  share_is_na <- is.na(unlist(select(out, starts_with("score"))))
+  expect_false(any(share_is_na))
+})
+
+test_that("is sensitive to low_threshold", {
+  companies <- slice(ictr_companies, 1)
+  inputs <- slice(ictr_inputs, 1:2)
+  out1 <- ictr(companies, inputs, low_threshold = .1)
+  out2 <- ictr(companies, inputs, low_threshold = .9)
+  expect_false(identical(out1, out2))
+})
+
+test_that("is sensitive to high_threshold", {
+  companies <- slice(ictr_companies, 1)
+  inputs <- slice(ictr_inputs, 1:2)
+  out1 <- ictr(companies, inputs, high_threshold = .1)
+  out2 <- ictr(companies, inputs, high_threshold = .9)
+  expect_false(identical(out1, out2))
+})
+
+test_that("if `companies` lacks crucial columns, errors gracefully", {
   companies <- slice(ictr_companies, 1)
   inputs <- slice(ictr_inputs, 1)
-  out <- ictr(companies, inputs)
-  expect_false(dplyr::is_grouped_df(out))
+
+  crucial <- "activity_uuid_product_uuid"
+  bad <- select(companies, -all_of(crucial))
+  expect_error(ictr(bad, inputs), crucial)
+
+  crucial <- "company_id"
+  bad <- select(companies, -all_of(crucial))
+  expect_error(ictr(bad, inputs), crucial)
+})
+
+test_that("if `inputs` lacks crucial columns, errors gracefully", {
+  companies <- slice(ictr_companies, 1)
+  inputs <- slice(ictr_inputs, 1)
+
+  crucial <- "activity_uuid_product_uuid"
+  bad <- select(inputs, -all_of(crucial))
+  expect_error(ictr(companies, bad), crucial)
+
+  crucial <- "input_co2"
+  bad <- select(inputs, -all_of(crucial))
+  expect_error(ictr(companies, bad), crucial)
+
+  crucial <- "unit"
+  bad <- select(inputs, -all_of(crucial))
+  expect_error(ictr(companies, bad), crucial)
+})
+
+test_that("with a missing value in `inputs$inputs_co2` errors gracefully", {
+  companies <- slice(ictr_companies, 1)
+  inputs <- slice(ictr_inputs, 1)
+  inputs$input_co2 <- NA
+  expect_error(ictr(companies, inputs), "input_co2.*missing")
+})
+
+test_that("if `companies` has 0-rows, returns a well structured 0-row output", {
+  inputs <- ictr_inputs
+
+  companies0 <- ictr_companies[0, ]
+  companies1 <- ictr_companies[1, ]
+  out0 <- ictr(companies0, inputs)
+  out1 <- ictr(companies1, inputs)
+
+  expect_s3_class(out0, "tbl_df")
+  expect_equal(names(out0), names(out1))
+  expect_equal(nrow(out0), 0)
+})
+
+test_that("if `inputs` has 0-rows, the output is normal (shares are NA)", {
+  companies <- slice(ictr_companies, 1)
+
+  inputs0 <- ictr_inputs[0, ]
+  inputs1 <- ictr_inputs[1, ]
+  out0 <- ictr(companies, inputs0)
+  out1 <- ictr(companies, inputs1)
+
+  expect_s3_class(out0, "tbl_df")
+  expect_equal(names(out0), names(out1))
+  expect_equal(nrow(out0), nrow(out1))
 })
