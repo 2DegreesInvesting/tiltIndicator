38b0224218235ec3891e027b6a8479e71e1039b7
Mauro Lepore <maurolepore@gmail.com>
2023-04-28 00:52:59 +0000

    refactor

--- tests/testthat/test-pstr.R
+++ tests/testthat/test-pstr.R
@@ -29,4 +29,21 @@ test_that("if a company has no products, shares are NA (#176)", {
   expect_equal(unique(out$score_aggregated), NA)
 })
 
+test_that("with a 0-row `copmanies` outputs a well structured 0-row tibble", {
+  out0 <- pstr(pstr_companies[0L, ], pstr_scenarios)
+  expect_s3_class(out0, "tbl")
+  expect_equal(nrow(out0), 0L)
 
+  out1 <- pstr(pstr_companies[1L, ], pstr_scenarios)
+  expect_s3_class(out1, "tbl")
+
+  expect_equal(names(out0), names(out1))
+})
+
+test_that("outputs correct values for edge cases", {
+  skip("FIXME: Adapt to new API")
+  edge_cases <- pstr_toy_weo_2022(reductions = c(NA, 30, 30.1, 70, 70.1))
+  with_reductions <- pstr_old_add_reductions(pstr_toy_companies(), pstr_toy_ep_weo(), edge_cases)
+  out <- pstr_add_transition_risk(with_reductions)
+  expect_equal(c("no_sector", "low", "medium", "medium", "high"), out$transition_risk)
+})
