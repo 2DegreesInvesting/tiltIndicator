27f1f7c514184833222e225f15d9267c8837e30a
Mauro Lepore <maurolepore@gmail.com>
2023-04-28 22:43:51 +0000

    Revert test-ictr.R

--- tests/testthat/test-ictr.R
+++ tests/testthat/test-ictr.R
@@ -23,31 +23,3 @@ test_that("outputs the expected columns", {
   out <- ictr(companies, inputs)
   expect_false(dplyr::is_grouped_df(out))
 })
-
-test_that("if a company matches no inputs, all shares are `NA` (#176)", {
-  companies <- tibble(
-    company_id = c("a", "c", "c", "d"),
-    activity_uuid_product_uuid = c("a", "x", "y", "z")
-  )
-  inputs <- tibble(
-    activity_uuid_product_uuid = c("b", "x"),
-    input_co2 = c(1, 3),
-    input_sector = c("transport", "transport"),
-    unit = c("metric ton*km", "metric ton*km"),
-  )
-
-  out <- ictr(companies, inputs)
-
-  summed <- out |>
-    group_by(id) |>
-    summarize(across(starts_with("score_"), sum))
-  ids_for_na <- summed$id[is.na(summed$score_all) &
-    is.na(summed$score_unit) &
-    is.na(summed$score_sector) &
-    is.na(summed$score_unit_sec)]
-
-  all_is_na <- all(is.na(unlist(out |>
-    filter(id %in% ids_for_na) |>
-    select(starts_with("score")))))
-  expect_true(all_is_na)
-})
