5a1ce94915c6e2b2e64c3d6a92fe0401bbe6c44b
Mauro Lepore <maurolepore@gmail.com>
2023-04-28 00:00:03 +0000

    Refactor

--- tests/testthat/test-pstr_prepare_companies.R
+++ tests/testthat/test-pstr_prepare_companies.R
@@ -1,7 +1,6 @@
 test_that("snapshot", {
   withr::local_options(list(readr.show_col_types = FALSE))
-
-  companies <- slice(readr::read_csv(extdata_path("pstr_raw_companies.csv")), 1)
+  companies <- slice(read_csv(extdata_path("pstr_raw_companies.csv")), 1)
   out <- pstr_prepare_companies(companies)
   expect_snapshot(format_robust_snapshot(out))
 })
