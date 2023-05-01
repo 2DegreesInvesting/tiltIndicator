da562ad7a03f9c699843e26766b3ec7bc34113be
Mauro Lepore <maurolepore@gmail.com>
2023-05-01 22:27:21 +0000

    Refactor

--- R/tmp.R
+++ R/tmp.R
@@ -1,3 +1,11 @@
+xctr_add_ranks <- function(data, x, .by) {
+  out <- data
+  for (i in seq_along(.by)) {
+    out <- add_rank(out, x, .by = .by[[i]])
+  }
+  out
+}
+
 add_rank <- function(data, x, .by) {
   if (identical(.by, "all")) {
     suffix <- "all"
@@ -13,12 +21,4 @@ add_rank <- function(data, x, .by) {
 
 rank_proportion <- function(x) {
   rank(x) / length(x)
-}
-
-xctr_add_ranks <- function(data, x, .by) {
-  out <- data
-  for (i in seq_along(.by)) {
-    out <- add_rank(out, x, .by = .by[[i]])
-  }
-  out
 }
