c6b4b8b59eabc24cf14d32ebb7275c36178612b6
Mauro Lepore <maurolepore@gmail.com>
2023-05-01 21:02:34 +0000

    refactor

--- R/tmp.R
+++ R/tmp.R
@@ -1,22 +1,13 @@
 
-add_rank <- function(data, col) {
-  nm <- paste0("perc_", col)
-  mutate(data, {{ nm }} := rank(.data[[col]]) / length(.data[[col]]))
+rank_proportion <- function(x) {
+  rank(x) / length(x)
 }
 
 ictr_add_ranks <- function(data) {
   data %>%
-    ## rank in comparison to all input products
-    mutate(perc_all = rank(.data$input_co2) / length(.data$input_co2)) |>
-    ## rank in comparison to all input products with same unit
-    group_by(.data$unit) |>
-    mutate(perc_unit = rank(.data$input_co2) / length(.data$input_co2)) %>%
-    ungroup() |>
-    ## rank in comparison to all input products with same input sector
-    group_by(.data$input_sector) |>
-    mutate(perc_sec = rank(.data$input_co2) / length(.data$input_co2)) %>%
-    ungroup() |>
-    ## rank in comparison to all input products with same unit and input sector
+    mutate(perc_all = rank_proportion(.data$input_co2)) |>
+    mutate(perc_unit = rank_proportion(.data$input_co2), .by = "unit") |>
+    mutate(perc_sec = rank_proportion(.data$input_co2), .by = "input_sector") %>%
     group_by(.data$unit, .data$input_sector) |>
     mutate(perc_unit_sec = rank(.data$input_co2) / length(.data$input_co2)) %>%
     ungroup()
