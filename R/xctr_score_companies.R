6d444248d22a0d1fcee8a6ec0e092063633f1a1f
Mauro Lepore <maurolepore@gmail.com>
2023-05-01 18:27:58 +0000

    Refactor

---
+++ R/xctr_score_companies.R
@@ -0,0 +1,33 @@
+xctr_score_companies <- function(ecoinvent_scores,
+                                 companies,
+                                 uuid = "activity_uuid_product_uuid",
+                                 benchmarks = c("all", "unit", "sector", "unit_sec")) {
+  stopifnot(hasName(companies, "company_id"))
+
+  companies_scores <- left_join(companies, ecoinvent_scores, by = c(uuid))
+
+  # For each company show all risk levels even if the share is 0.
+  dt_sceleton <- tibble(
+    company_id = rep(unique(companies_scores$company_id), each = 3),
+    score = rep(c("high", "medium", "low"), length(unique(companies_scores$company_id))),
+  )
+
+  # Share in comparison to all inputs and those with same unit, sector, ...
+  benchmarks_list <- map(benchmarks, ~ add_share(companies_scores, .x))
+
+  ictr_output <- append(list(dt_sceleton), benchmarks_list) |>
+    reduce(left_join, by = c("company_id", "score"))
+
+  ictr_output |>
+    mutate(
+      across(starts_with("share_"), na_to_0_if_not_all_is_na),
+      .by = "company_id"
+    )
+}
+
+na_to_0_if_not_all_is_na <- function(x) {
+  if (all(is.na(x))) {
+    return(x)
+  }
+  replace_na(x, 0)
+}
