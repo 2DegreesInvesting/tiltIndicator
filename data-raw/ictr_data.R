6728bd2ccbf0e7d42901e4e406a8699b8b782592
Kalash Singhal <kalash@2degrees-investing.org>
2023-05-03 15:12:16 +0200

    source comment update

--- data-raw/ictr_data.R
+++ data-raw/ictr_data.R
@@ -4,11 +4,14 @@ library(usethis)
 
 load_all()
 
-# The raw data comes from tiltIndicator#217, which includes pre-processing
+# Source:
+# https://github.com/2DegreesInvesting/tiltIndicator/issues/167#issuecomment-1527223646
 ictr_inputs <- read_csv(here("data-raw", "ictr", "ictr_inputs.csv")) |>
   select(all_of(ictr_inputs_crucial()))
 use_data(ictr_inputs, overwrite = TRUE)
 
+# Source:
+# https://github.com/2DegreesInvesting/tiltIndicator/issues/167#issuecomment-1527223646
 ictr_companies <- read_csv(here("data-raw", "ictr", "ictr_companies.csv")) |>
   select(all_of(ictr_companies_crucial()))
 use_data(ictr_companies, overwrite = TRUE)
--- data-raw/pctr_data.R
+++ data-raw/pctr_data.R
@@ -5,15 +5,15 @@ library(usethis)
 
 load_all()
 
-# Source: Tilman via tilt.indicator.product.carbon.tr-main_v2.zip at:
-# https://github.com/2DegreesInvesting/tiltData/issues/200#issue-1623506353
+# Source:
+# https://github.com/2DegreesInvesting/tiltIndicator/issues/167#issuecomment-1527223646
 pctr_ecoinvent_co2 <- here("data-raw/pctr/data_ecoinvent_co2.csv") |>
   read_csv(show_col_types = FALSE) |>
   select(all_of(pctr_ecoinvent_co2_crucial()))
 use_data(pctr_ecoinvent_co2, overwrite = TRUE)
 
-# Source: Tilman via tilt.indicator.product.carbon.tr-main_v2.zip at:
-# https://github.com/2DegreesInvesting/tiltData/issues/200#issue-1623506353
+# Source:
+# https://github.com/2DegreesInvesting/tiltIndicator/issues/167#issuecomment-1527223646
 pctr_companies <- here("data-raw/pctr/tilt_companies.csv") |>
   read_csv(show_col_types = FALSE) |>
   select(all_of(pctr_companies_crucial()))
