b8fa648a0003f16dc1d603961cc372ed9fc3e1c4
Mauro Lepore <maurolepore@gmail.com>
2023-05-03 19:58:10 +0000

    refactor

--- R/istr.R
+++ R/istr.R
@@ -83,3 +83,9 @@ istr_at_company_level <- function(data, companies) {
 
   xstr_polish_output(out)
 }
+
+istr_add_reductions <- function(companies, weo_2022) {
+  companies |>
+    # left_join(ep_weo, by = c("eco_sectors" = "ECO_sector")) |>
+    left_join(weo_2022, by = c("weo_product_mapper" = "product", "weo_flow_mapper" = "flow"))
+}
--- R/istr_add_reductions.R
+++
@@ -1,5 +0,0 @@
-istr_add_reductions <- function(companies, weo_2022) {
-  companies |>
-    # left_join(ep_weo, by = c("eco_sectors" = "ECO_sector")) |>
-    left_join(weo_2022, by = c("weo_product_mapper" = "product", "weo_flow_mapper" = "flow"))
-}
