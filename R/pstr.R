323d7cbc6ce4f6515c68fe29164e06d95069b6b1
Mauro Lepore <maurolepore@gmail.com>
2023-05-03 19:44:39 +0000

    refactor-pstr

--- R/pstr.R
+++ R/pstr.R
@@ -22,3 +22,31 @@ pstr <- function(companies, scenarios) {
     pstr_at_product_level(scenarios) |>
     pstr_at_company_level(companies)
 }
+
+#' @rdname pstr
+#' @export
+pstr_at_product_level <- function(companies, scenarios) {
+  companies |>
+    pstr_add_reductions(scenarios) |>
+    pstr_add_transition_risk()
+}
+
+pstr_add_reductions <- function(companies, scenarios) {
+  left_join(
+    companies, scenarios,
+    by = join_by("type", "sector", "subsector"),
+    relationship = "many-to-many"
+  )
+}
+
+pstr_add_transition_risk <- function(with_reductions) {
+  with_reductions |>
+    mutate(
+      transition_risk = case_when(
+        reductions <= 30 ~ "low",
+        reductions > 30 & reductions <= 70 ~ "medium",
+        reductions > 70 ~ "high",
+        TRUE ~ "no_sector",
+      )
+    )
+}
