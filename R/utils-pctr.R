dc9148143391dc5c2fda5ed2e1349013f756fda2
Mauro Lepore <maurolepore@gmail.com>
2023-05-08 00:30:24 +0000

    refactor

--- R/utils-pctr.R
+++ R/utils-pctr.R
@@ -1,19 +1,3 @@
-pctr_ecoinvent_co2_crucial <- function() {
-  crucial_in_pctr_at_product_level <- c(
-    "co2_footprint",
-    "tilt_sector",
-    "unit",
-    "isic_4digit"
-  )
-
-  crucial_in_pctr_at_company_level <- c(
-    "activity_uuid_product_uuid",
-    "ei_activity_name"
-  )
-
-  c(crucial_in_pctr_at_product_level, crucial_in_pctr_at_company_level)
-}
-
 pctr_at_company_level_crucial <- function() {
   c(
     "activity_product_uuid",
--- data-raw/products.R
+++ data-raw/products.R
@@ -8,5 +8,12 @@ devtools::load_all()
 # https://github.com/2DegreesInvesting/tiltIndicator/issues/167#issuecomment-1527223646
 products <- here("data-raw", "products.csv") |>
   read_csv(show_col_types = FALSE) |>
-  select(all_of(pctr_ecoinvent_co2_crucial()))
+  select(all_of(c(
+    "co2_footprint",
+    "tilt_sector",
+    "unit",
+    "isic_4digit",
+    "activity_uuid_product_uuid",
+    "ei_activity_name"
+  )))
 use_data(products, overwrite = TRUE)
