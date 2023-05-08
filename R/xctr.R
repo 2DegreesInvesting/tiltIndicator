276bfeddd5af0638d67947334ea4ff6e1ecea84d
Mauro Lepore <maurolepore@gmail.com>
2023-05-08 01:26:51 +0000

    refactor

--- R/xctr.R
+++ R/xctr.R
@@ -150,32 +150,6 @@ na_to_0_if_not_all_is_na <- function(x) {
   replace_na(x, 0)
 }
 
-xctr_score_companies <- function(companies,
-                                 co2,
-                                 uuid = "activity_uuid_product_uuid",
-                                 .by = c("all", "unit", "sector", "unit_sec")) {
-  stopifnot(hasName(companies, "company_id"))
-
-  companies_scores <- left_join(companies, co2, by = c(uuid))
-
-  # For each company show all risk levels even if the share is 0.
-  dt_sceleton <- tibble(
-    company_id = rep(unique(companies_scores$company_id), each = 3),
-    score = rep(c("high", "medium", "low"), length(unique(companies_scores$company_id))),
-  )
-
-  # Share in comparison to all inputs and those with same unit, sector, ...
-  benchmarks_list <- map(.by, ~ add_share(companies_scores, .x))
-
-  list(dt_sceleton) |>
-    append(benchmarks_list) |>
-    reduce(left_join, by = c("company_id", "score")) |>
-    mutate(
-      across(starts_with("share_"), na_to_0_if_not_all_is_na),
-      .by = "company_id"
-    )
-}
-
 xctr_add_ranks <- function(data, x) {
   .by <- xctr_benchmarks()
   out <- distinct(data)
