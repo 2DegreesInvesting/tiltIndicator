b068736852aaf087536179cb2fb8083cd54b24f3
Mauro Lepore <maurolepore@gmail.com>
2023-05-08 00:33:34 +0000

    refactor

--- R/utils-ictr.R
+++
@@ -1,8 +0,0 @@
-ictr_companies_crucial <- function() {
-  c(
-    "activity_uuid_product_uuid",
-    "clustered",
-    "company_id",
-    "unit"
-  )
-}
--- data-raw/companies.R
+++ data-raw/companies.R
@@ -4,5 +4,10 @@ library(usethis)
 # Source:
 # https://github.com/2DegreesInvesting/tiltIndicator/issues/167#issuecomment-1527223646
 companies <- read_csv(here("data-raw", "companies.csv")) |>
-  select(all_of(ictr_companies_crucial()))
+  select(all_of(c(
+    "activity_uuid_product_uuid",
+    "clustered",
+    "company_id",
+    "unit"
+  )))
 use_data(companies, overwrite = TRUE)
