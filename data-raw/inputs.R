cb047b79beee860472b26341a6f933d7133fedc5
Mauro Lepore <maurolepore@gmail.com>
2023-05-08 00:31:38 +0000

    inputs

--- data-raw/inputs.R
+++ data-raw/inputs.R
@@ -5,9 +5,9 @@ devtools::load_all()
 
 # Source:
 # https://github.com/2DegreesInvesting/tiltIndicator/issues/167#issuecomment-1527223646
-ictr_inputs <- read_csv(here("data-raw", "inputs.csv")) |>
+inputs <- read_csv(here("data-raw", "inputs.csv")) |>
   select(all_of(ictr_inputs_crucial()))
 
-is_na_free <- !anyNA(ictr_inputs$input_co2_footprint)
+is_na_free <- !anyNA(inputs$input_co2_footprint)
 stopifnot(is_na_free)
-use_data(ictr_inputs, overwrite = TRUE)
+use_data(inputs, overwrite = TRUE)
