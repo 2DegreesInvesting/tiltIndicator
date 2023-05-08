ef4760bb3cf75a5111526350483962c1ada4bb66
Mauro Lepore <maurolepore@gmail.com>
2023-05-08 00:19:09 +0000

    prune-copmanies

--- data-raw/ictr_data.R
+++ data-raw/ictr_data.R
@@ -11,9 +11,3 @@ ictr_inputs <- read_csv(here("data-raw", "ictr", "ictr_inputs.csv")) |>
 is_na_free <- !anyNA(ictr_inputs$input_co2_footprint)
 stopifnot(is_na_free)
 use_data(ictr_inputs, overwrite = TRUE)
-
-# Source:
-# https://github.com/2DegreesInvesting/tiltIndicator/issues/167#issuecomment-1527223646
-ictr_companies <- read_csv(here("data-raw", "ictr", "ictr_companies.csv")) |>
-  select(all_of(ictr_companies_crucial()))
-use_data(ictr_companies, overwrite = TRUE)
