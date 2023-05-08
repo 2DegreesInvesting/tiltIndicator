6efe0b744acc3dc6b519c6e8c2d383a8a064a55b
Mauro Lepore <maurolepore@gmail.com>
2023-05-08 00:32:48 +0000

    refactor

--- data-raw/inputs.R
+++ data-raw/inputs.R
@@ -6,7 +6,14 @@ devtools::load_all()
 # Source:
 # https://github.com/2DegreesInvesting/tiltIndicator/issues/167#issuecomment-1527223646
 inputs <- read_csv(here("data-raw", "inputs.csv")) |>
-  select(all_of(ictr_inputs_crucial()))
+  select(all_of(c(
+    "input_co2_footprint",
+    "input_tilt_sector",
+    "input_unit",
+    "input_isic_4digit",
+    "input_activity_uuid_product_uuid",
+    "activity_uuid_product_uuid"
+  )))
 
 is_na_free <- !anyNA(inputs$input_co2_footprint)
 stopifnot(is_na_free)
