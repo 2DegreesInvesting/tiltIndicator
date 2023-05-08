1e7598dbbff55e04ff0ee33fe047d05748664a87
Mauro Lepore <maurolepore@gmail.com>
2023-05-08 20:37:59 -0300

    * At product level XCTR now drops NA from unmatched products (#267)

--- tests/testthat/test-xctr_at_product_level.R
+++ tests/testthat/test-xctr_at_product_level.R
@@ -13,3 +13,21 @@ test_that("outputs expected columns at product level", {
   expected <- c(cols_at_product_level(), "co2_footprint")
   expect_named(out, expected)
 })
+
+test_that("unmatched products don't introduce NA's (#266)", {
+  companies <- tibble(
+    company_id = "x",
+    activity_uuid_product_uuid = c("a", "b"),
+    clustered = c("1", "2")
+  )
+  inputs <- tibble(
+    activity_uuid_product_uuid = c("a"),
+    input_activity_uuid_product_uuid = c("m"),
+    input_co2_footprint = 1,
+    input_tilt_sector = "transport",
+    input_unit = "metric ton*km",
+    input_isic_4digit = "4575"
+  )
+  out <- xctr_at_product_level(companies, inputs)
+  expect_false(anyNA(out$risk_category))
+})
