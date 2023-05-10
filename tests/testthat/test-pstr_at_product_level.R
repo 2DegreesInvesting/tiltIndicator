738f5ffad152116b4017e2cae045ca8f5fd8e102
Mauro Lepore <maurolepore@gmail.com>
2023-05-10 22:59:15 +0000

    refactor

--- tests/testthat/test-pstr_at_product_level.R
+++ tests/testthat/test-pstr_at_product_level.R
@@ -1,19 +1,6 @@
 test_that("outputs expected columns at product level", {
   companies <- slice(pstr_companies, 1)
   scenarios <- slice(pstr_scenarios, 1)
-
-  expected <- c(
-    "companies_id",
-    "grouped_by",
-    "risk_category",
-    "clustered",
-    "activity_uuid_product_uuid",
-    "tilt_sector",
-    "tilt_subsector",
-    "scenario",
-    "year",
-    "type"
-  )
   out <- pstr_at_product_level(companies, scenarios)
-  expect_named(out, expected)
+  expect_named(out, pstr_cols_at_product_level())
 })
