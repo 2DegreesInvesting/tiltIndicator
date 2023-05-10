799372cff0eb10a38ef2be404899167a80cc8e3e
Kalash Singhal <kalash@2degrees-investing.org>
2023-05-10 13:20:50 +0200

    update PSTR datasets

--- R/pstr_prepare_companies.R
+++ R/pstr_prepare_companies.R
@@ -1,14 +1,10 @@
 #' Given a messy PSTR `companies` dataframe returns a cleaner one
 #'
 #' @param data A dataframe with these columns:
-#' * ipr_sector.x
-#' * ipr_sector.y
-#' * ipr_subsector.x
-#' * ipr_subsector.y
-#' * weo_product.x
-#' * weo_product.y
-#' * weo_flow.x
-#' * weo_flow.y
+#' * ipr_sector
+#' * ipr_subsector
+#' * weo_product
+#' * weo_flow
 #'
 #' @family pre-processing helpers
 #'
@@ -19,27 +15,15 @@
 #' library(dplyr, warn.conflicts = FALSE)
 #' library(readr, warn.conflicts = FALSE)
 #'
-#' raw_companies <- read_csv(extdata_path("pstr_raw_companies.csv"))
+#' raw_companies <- read_csv(extdata_path("pstr_companies.csv"))
 #' glimpse(raw_companies)
 #'
 #' companies <- pstr_prepare_companies(raw_companies)
 #' companies
 pstr_prepare_companies <- function(data) {
   data |>
-    merge_scenario_columns() |>
     lowercase_characters() |>
     pivot_type_sector_subsector()
-}
-
-merge_scenario_columns <- function(data) {
-  data |>
-    mutate(ipr_sector = if_else(is.na(.data$ipr_sector.y), .data$ipr_sector.x, .data$ipr_sector.y)) |>
-    mutate(ipr_subsector = if_else(is.na(.data$ipr_subsector.y), .data$ipr_subsector.x, .data$ipr_subsector.y)) |>
-    mutate(weo_product = if_else(is.na(.data$weo_product.y), .data$weo_product.x, .data$weo_product.y)) |>
-    mutate(weo_flow = if_else(is.na(.data$weo_flow.y), .data$weo_flow.x, .data$weo_flow.y)) |>
-    select(-ends_with(".x")) |>
-    select(-ends_with(".y")) |>
-    distinct()
 }
 
 pivot_type_sector_subsector <- function(companies) {
