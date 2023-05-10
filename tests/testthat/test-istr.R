e89af11b35c1af35cb191d520fee390afee17a68
Mauro Lepore <maurolepore@gmail.com>
2023-05-10 19:28:40 +0000

    test

--- tests/testthat/test-istr.R
+++ tests/testthat/test-istr.R
@@ -8,3 +8,12 @@ test_that("outputs expected columns at company level", {
   expected <- cols_at_company_level()
   expect_equal(names(out)[seq_along(expected)], expected)
 })
+
+test_that("with a missing value in the co2* column errors gracefully", {
+  companies <- slice(istr_companies, 1)
+  scenario <- slice(istr_weo_2022, 1)
+  scenario$reductions <- NA
+  ep_weo <- slice(istr_ep_weo, 1)
+
+  expect_error(istr(companies, scenario, ep_weo), "reductions")
+})
