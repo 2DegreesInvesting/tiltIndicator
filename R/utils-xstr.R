279971e673f60b72eb12d1490226b0970ec54127
Mauro Lepore <maurolepore@gmail.com>
2023-05-10 20:20:38 +0000

    Remove-dead-code

--- R/utils-xstr.R
+++ R/utils-xstr.R
@@ -6,13 +6,6 @@ xstr_polish_output_at_product_level <- function(data) {
     relocate(all_of(cols_at_all_levels()))
 }
 
-xstr_polish_output_at_company_level <- function(data) {
-  data |>
-    unite("grouped_by", "scenario", "year", remove = FALSE) |>
-    rename(value = "score_aggregated") |>
-    relocate(all_of(cols_at_company_level()))
-}
-
 xstr_check <- function(scenarios) {
   check_has_no_na(scenarios, "reductions")
 }
