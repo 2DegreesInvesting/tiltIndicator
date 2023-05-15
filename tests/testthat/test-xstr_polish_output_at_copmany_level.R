bf8c0fc2f19ce2cefc700ec70c758d06a1c5a342
Mauro Lepore <maurolepore@gmail.com>
2023-05-15 22:48:51 +0000

    refactor

--- tests/testthat/test-xstr_polish_output_at_copmany_level.R
+++ tests/testthat/test-xstr_polish_output_at_copmany_level.R
@@ -1,10 +1,10 @@
 test_that("outputs the expected columns", {
   # styler: off
   pstr <- tribble(
-    ~companies_id,                              ~grouped_by, ~risk_category, ~value,
-    "fleischerei-stiefsohn_00000005219477-001", "ipr_1.5c required policy scenario_2020",         "high",      0,
-    "fleischerei-stiefsohn_00000005219477-001", "ipr_1.5c required policy scenario_2020",       "medium",      0,
-    "fleischerei-stiefsohn_00000005219477-001", "ipr_1.5c required policy scenario_2020",          "low",      1
+    ~companies_id,           ~grouped_by, ~risk_category, ~value,
+              "a", "ipr_some thing_2020",         "high",      0,
+              "a", "ipr_some thing_2020",       "medium",      0,
+              "a", "ipr_some thing_2020",          "low",      1
   )
   # styler: on
   out <- pstr_polish_output_at_copmany_level(pstr)
