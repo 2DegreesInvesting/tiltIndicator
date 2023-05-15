8ee37d2744dcc680224676177d2a23e557cd7f41
Mauro Lepore <maurolepore@gmail.com>
2023-05-15 22:49:26 +0000

    refactor

--- tests/testthat/test-xstr_polish_output_at_copmany_level.R
+++ tests/testthat/test-xstr_polish_output_at_copmany_level.R
@@ -7,7 +7,9 @@ test_that("outputs the expected columns", {
               "a", "ipr_some thing_2020",          "low",      1
   )
   # styler: on
+
   out <- pstr_polish_output_at_copmany_level(pstr)
+
   exp <- c("companies_id", "type", "scenario", "year", "risk_category", "value")
   expect_named(out, exp)
 })
