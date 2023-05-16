351f3e11b4e53accb5d8355d9f829d97fddd6bd0
Mauro Lepore <maurolepore@gmail.com>
2023-05-16 20:26:24 +0000

    refactor

--- tests/testthat/test-xctr.R
+++ tests/testthat/test-xctr.R
@@ -188,7 +188,6 @@ test_that("if `co2` lacks crucial columns, errors gracefully", {
 })
 
 test_that("with a 0-row `co2` outputs a well structured 0-row tibble", {
-  skip("FIXME")
   companies <- slice(companies, 1)
   out <- xctr(companies, products[0, ])
   expect_equal(out, ptype_at_company_level(companies$company_id))
