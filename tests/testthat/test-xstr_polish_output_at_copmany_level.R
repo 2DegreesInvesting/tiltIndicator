8e36961b1934b029d624c919609ff94bf0bac3b4
Mauro Lepore <maurolepore@gmail.com>
2023-05-16 07:28:43 -0300

    * New `pstr_polish_output_at_company_level()` (#327)
    Closes #320
    Relates to #199

---
+++ tests/testthat/test-xstr_polish_output_at_copmany_level.R
@@ -0,0 +1,15 @@
+test_that("outputs the expected columns", {
+  # styler: off
+  pstr <- tribble(
+    ~companies_id,           ~grouped_by, ~risk_category, ~value,
+              "a", "ipr_some thing_2020",         "high",      0,
+              "a", "ipr_some thing_2020",       "medium",      0,
+              "a", "ipr_some thing_2020",          "low",      1
+  )
+  # styler: on
+
+  out <- pstr_polish_output_at_copmany_level(pstr)
+
+  exp <- c("companies_id", "type", "scenario", "year", "risk_category", "value")
+  expect_named(out, exp)
+})
