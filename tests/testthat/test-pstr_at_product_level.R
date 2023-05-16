c8acc85b16f89a79369136fd0cf2f5815b9fa1e4
delacombazlinda <delacombaz.linda@gmail.com>
2023-05-16 11:53:46 +0000

    remove test at product level

--- tests/testthat/test-pstr_at_product_level.R
+++ tests/testthat/test-pstr_at_product_level.R
@@ -4,13 +4,3 @@ test_that("outputs expected columns at product level", {
   out <- pstr_at_product_level(companies, scenarios)
   expect_named(out, pstr_cols_at_product_level())
 })
-
-test_that("the thresholds are in the range 0 to 1", {
-  pstr_arguments <- formals(pstr_at_product_level)
-
-  low_threshold <- eval(pstr_arguments$low_threshold)
-  high_threshold <- eval(pstr_arguments$high_threshold)
-
-  expect_true(low_threshold >= 0 & low_threshold <= 1)
-  expect_true(high_threshold >= 0 & high_threshold <= 1)
-})
