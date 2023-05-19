abca1a72bad859ebd2d2b78b7c5ee561ff96630b
Mauro Lepore <maurolepore@gmail.com>
2023-05-19 14:40:19 +0000

    New risk_category_levels()

--- R/utils.R
+++ R/utils.R
@@ -47,6 +47,10 @@ is_xctr <- function(data) {
   matches_name(data, col_to_rank(data))
 }
 
+risk_category_levels <- function() {
+  c("high", "medium", "low")
+}
+
 get_column <- function(data, pattern) {
   data[[extract_name(data, pattern)]]
 }
