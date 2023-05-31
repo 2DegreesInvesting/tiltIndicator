9a56651b69a70dc0cc5051297b996e9ade159b7c
Mauro Lepore <maurolepore@gmail.com>
2023-05-31 19:15:43 -0300

    Rename `pstr_prepare_scenario()` to `xctr_*()` (#385)

--- tests/testthat/test-pstr_prepare_scenario.R
+++ tests/testthat/test-pstr_prepare_scenario.R
@@ -5,7 +5,7 @@ test_that("outputs the expected snapshot", {
   weo <- read_csv(extdata_path("pstr_weo_2022.csv")) |> slice(1:3)
   ipr <- read_csv(extdata_path("pstr_ipr_2022.csv")) |> slice(1:3)
   scenarios <- list(weo = weo, ipr = ipr)
-  out <- pstr_prepare_scenario(scenarios) |>
+  out <- xstr_prepare_scenario(scenarios) |>
     select(-scenario, -region, -year)
 
   expect_snapshot(out)
@@ -17,6 +17,6 @@ test_that("takes any number of scenarios", {
   weo <- read_csv(extdata_path("pstr_weo_2022.csv")) |> slice(1:3)
   scenarios <- list(s1 = weo, s2 = weo, s3 = weo)
 
-  out <- pstr_prepare_scenario(scenarios)
+  out <- xstr_prepare_scenario(scenarios)
   expect_equal(unique(out$type), c("s1", "s2", "s3"))
 })
