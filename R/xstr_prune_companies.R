ce5dc2ee96a476e158315089f294add4ff7e5b05
Mauro Lepore <maurolepore@gmail.com>
2023-05-31 17:26:47 -0300

    `xstr_prune_companies()` now expects the column `company_id` (#380)

--- R/xstr_prune_companies.R
+++ R/xstr_prune_companies.R
@@ -13,34 +13,36 @@
 #' @examples
 #' # styler: off
 #' companies <- tibble::tribble(
-#'   ~row_id, ~companies_id, ~clustered, ~activity_uuid_product_uuid, ~tilt_sector,
+#'   ~row, ~company_id, ~clustered, ~activity_uuid_product_uuid, ~tilt_sector,
 #'   # Keep: Has product info
-#'         1,           "a",       "b1",                        "c1",          "x",
+#'      1,         "a",       "b1",                        "c1",          "x",
 #'   # Drop: Lacks product info and sector info is duplicated
-#'         2,           "a",         NA,                          NA,          "x",
-#'   # Drop: Lacks product info and sector info is duplicated
-#'         3,           "a",         NA,                          NA,          "x",
+#'      2,         "a",         NA,                          NA,          "x",
 #'   # Keep: Lacks product info but sector info is unique
-#'         4,           "a",         NA,                          NA,          "y",
+#'      3,         "a",         NA,                          NA,          "y",
 #'   # Drop: Lacks product info and sector info is duplicated
-#'         5,           "a",         NA,                          NA,          "y",
-#'   # Keep: Has product info
-#'         6,           "a",       "b6",                        "c6",          "z",
-#'   # Keep: Has product info
-#'         7,           "a",       "b7",                          NA,          "z",
+#'      4,         "a",         NA,                          NA,          "y",
 #' )
 #' # styler: on
 #'
 #' xstr_prune_companies(companies)
 xstr_prune_companies <- function(data) {
+  check_prune_companies(data)
+
   data |>
     flag_companies() |>
     pick_companies()
 }
 
+check_prune_companies <- function(data) {
+  check_crucial_names(data, c(
+    "company_id", "clustered", "activity_uuid_product_uuid", "tilt_sector"
+  ))
+}
+
 flag_companies <- function(data) {
   data |>
-    group_by(.data$companies_id, .data$tilt_sector) |>
+    group_by(.data$company_id, .data$tilt_sector) |>
     mutate(odd = ifelse(
       is.na(.data$clustered) & is.na(.data$activity_uuid_product_uuid),
       TRUE,
