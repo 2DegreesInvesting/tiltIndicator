cd9c23636a401cefdd0c506fc96e40c5e47ccecf
Mauro Lepore <maurolepore@gmail.com>
2023-06-20 15:15:40 +0000

    Failing test

--- tests/testthat/test-xstr_prepare_scenario.R
+++ tests/testthat/test-xstr_prepare_scenario.R
@@ -19,3 +19,14 @@ test_that("takes any number of scenarios", {
   out <- xstr_prepare_scenario(scenarios)
   expect_equal(unique(out$type), c("s1", "s2", "s3"))
 })
+
+test_that("with duplicated scenario+year+sector+subsector errors gracefully (#391)", {
+  withr::local_options(list(readr.show_col_types = FALSE))
+
+  weo <- slice(read_csv(extdata_path("str_weo_targets.csv")), 1)
+  duplicated <- mutate(bind_rows(weo, weo), co2_reductions = 1:2)
+  expect_error(
+    xstr_prepare_scenario(list(weo = duplicated)),
+    "`scenario` must be unique by `scenario`, `year`, `sector`, and `subsector`."
+  )
+})
