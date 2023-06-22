0f2e68f4b3b04d9118164b9e2b39867077baf70e
Mauro Lepore <maurolepore@gmail.com>
2023-06-22 00:58:49 +0000

    refactor

--- tests/testthat/test-pstr.R
+++ tests/testthat/test-pstr.R
@@ -288,6 +288,8 @@ test_that("some match yields 3 rows with no NA (#393)", {
   out <- pstr(companies, scenarios)
 
   expect_equal(nrow(out), 3L)
+  n <- length(unique(out$grouped_by)) * length(unique(out$risk_category))
+  expect_equal(n, 3L)
   expect_false(anyNA(out))
 })
 
