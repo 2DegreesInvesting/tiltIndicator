e5810c72cd08d9a820a4d23b57f20151f254b926
Mauro Lepore <maurolepore@gmail.com>
2023-06-23 23:22:17 +0000

    refactor

--- R/istr_at_product_level.R
+++ R/istr_at_product_level.R
@@ -1,7 +1,8 @@
-#' @rdname pstr
+#' @rdname istr
 #' @export
-pstr_at_product_level <- function(companies,
+istr_at_product_level <- function(companies,
                                   scenarios,
+                                  inputs,
                                   low_threshold = ifelse(scenarios$year == 2030, 1 / 9, 1 / 3),
                                   high_threshold = ifelse(scenarios$year == 2030, 2 / 9, 2 / 3)) {
   xstr_check(companies, scenarios)
@@ -9,21 +10,25 @@ pstr_at_product_level <- function(companies,
   .companies <- prepare_companies(companies)
   .scenarios <- prepare_scenarios(scenarios, low_threshold, high_threshold)
 
-  .companies |>
+  inputs |>
+    distinct() |>
     xstr_add_values_to_categorize(.scenarios) |>
     add_risk_category(low_threshold, high_threshold, .default = NA) |>
+    xctr_join_companies(.companies) |>
     xstr_polish_output_at_product_level() |>
-    pstr_select_cols_at_product_level() |>
+    istr_select_cols_at_product_level() |>
     polish_output(cols_at_product_level())
 }
 
-pstr_select_cols_at_product_level <- function(data) {
-  select(data, all_of(pstr_cols_at_product_level()))
+istr_select_cols_at_product_level <- function(data) {
+  select(data, all_of(istr_cols_at_product_level()))
 }
 
-pstr_cols_at_product_level <- function() {
+istr_cols_at_product_level <- function() {
   c(
     xstr_cols_at_product_level(),
-    "tilt_subsector"
+    "input_activity_uuid_product_uuid",
+    "input_tilt_sector",
+    "input_tilt_subsector"
   )
 }
