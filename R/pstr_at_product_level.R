ee7fe97706eabbe768a922def6192e8703db09d8
Mauro Lepore <maurolepore@gmail.com>
2023-06-23 23:12:11 +0000

    Refactor

---
+++ R/pstr_at_product_level.R
@@ -0,0 +1,58 @@
+#' @rdname pstr
+#' @export
+pstr_at_product_level <- function(companies,
+                                  scenarios,
+                                  low_threshold = ifelse(scenarios$year == 2030, 1 / 9, 1 / 3),
+                                  high_threshold = ifelse(scenarios$year == 2030, 2 / 9, 2 / 3)) {
+  xstr_check(companies, scenarios)
+  stop_if_all_sector_and_subsector_are_na_for_some_type(scenarios)
+
+  .scenarios <- prepare_scenarios(scenarios, low_threshold, high_threshold)
+  .companies <- prepare_companies(companies)
+
+  .companies |>
+    xstr_add_values_to_categorize(.scenarios) |>
+    add_risk_category(low_threshold, high_threshold, .default = NA) |>
+    xstr_polish_output_at_product_level() |>
+    pstr_select_cols_at_product_level() |>
+    polish_output(cols_at_product_level())
+}
+
+xstr_add_values_to_categorize <- function(data, scenarios) {
+  left_join(
+    data, scenarios,
+    by = join_by("type", "sector", "subsector"),
+    relationship = "many-to-many"
+  )
+}
+
+stop_if_all_sector_and_subsector_are_na_for_some_type <- function(scenarios) {
+  bad_type <- scenarios |>
+    summarize(
+      all_na = all(is.na(.data$sector) & is.na(.data$subsector)), .by = "type"
+    ) |>
+    filter(.data$all_na) |>
+    pull(.data$type)
+
+  has_bad_type <- !identical(bad_type, character(0))
+  if (has_bad_type) {
+    bad <- toString(bad_type)
+    abort(c(
+      "Each scenario `type` must have some `sector` and `subsector`.",
+      x = glue("All `sector` and `subsector` are missing for `type` {bad}."),
+      i = "Did you need to prepare the data with `xstr_prepare_scenario()`?"
+    ))
+  }
+  invisible(scenarios)
+}
+
+pstr_select_cols_at_product_level <- function(data) {
+  select(data, all_of(pstr_cols_at_product_level()))
+}
+
+pstr_cols_at_product_level <- function() {
+  c(
+    xstr_cols_at_product_level(),
+    "tilt_subsector"
+  )
+}
