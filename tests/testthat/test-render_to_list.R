414c0c15e8b87438cce7dec550663583de707d1b
Mauro Lepore <maurolepore@gmail.com>
2023-06-23 23:42:17 +0000

    refactor

--- tests/testthat/test-render_to_list.R
+++
@@ -1,7 +0,0 @@
-test_that("outputs a named list of one tibble element per output.csv", {
-  path <- system.file("extdata", "mtcars.Rmd", package = "tiltIndicator")
-  out <- render_to_list(path)
-  expect_type(out, "list")
-  expect_true(any(grepl("data", names(out))))
-  expect_equal(out[["data"]], mtcars)
-})
