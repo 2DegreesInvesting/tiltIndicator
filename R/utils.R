06f2d46d05d4e3489dd2e43d8e8bf07ca3898dba
Mauro Lepore <maurolepore@gmail.com>
2023-06-26 00:27:22 +0000

    refactor

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,10 @@
 document_value <- function() {
   paste0(
-    "At product level, a dataframe with at least columns ",
+    "A data frame with columns `companies_id` and nested columns `product` and",
+    " `company` holding the outputs at product and company level. Unnesting ",
+    "`product` yields a data frame with at least columns ",
     toString(cols_at_all_levels()), ". ",
-    "At company level, a dataframe with at least columns ",
+    "Unnesting `company` yields a data frame with at least columns ",
     toString(cols_at_company_level()), "."
   )
 }
