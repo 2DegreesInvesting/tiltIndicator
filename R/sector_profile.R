243ce163e7275ff7af2ee0dbc3ab526e85af4fca
Mauro Lepore <maurolepore@gmail.com>
2023-08-17 17:48:41 +0000

    fix

--- R/sector_profile.R
+++ R/sector_profile.R
@@ -30,7 +30,7 @@ sector_profile <- function(companies,
                            scenarios,
                            low_threshold = ifelse(scenarios$year == 2030, 1 / 9, 1 / 3),
                            high_threshold = ifelse(scenarios$year == 2030, 2 / 9, 2 / 3)) {
-  product <- pstr_at_product_level(companies, scenarios, low_threshold, high_threshold)
-  company <- xctr_at_company_level(product)
+  product <- pstr_product(companies, scenarios, low_threshold, high_threshold)
+  company <- xctr_company(product)
   nest_levels(product, company)
 }
