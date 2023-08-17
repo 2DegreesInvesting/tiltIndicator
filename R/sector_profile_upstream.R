c8d495becbac97c0363f4717a50f521f80d55889
Mauro Lepore <maurolepore@gmail.com>
2023-08-17 17:50:42 +0000

    Use new name

--- R/sector_profile_upstream.R
+++ R/sector_profile_upstream.R
@@ -30,9 +30,8 @@ sector_profile_upstream <- function(companies,
                                     inputs,
                                     low_threshold = ifelse(scenarios$year == 2030, 1 / 9, 1 / 3),
                                     high_threshold = ifelse(scenarios$year == 2030, 2 / 9, 2 / 3)) {
-  product <- istr_at_product_level(
-    companies, scenarios, inputs, low_threshold, high_threshold
-  )
-  company <- xctr_at_company_level(product)
+  product <- companies |>
+    istr_product(scenarios, inputs, low_threshold, high_threshold)
+  company <- xctr_company(product)
   nest_levels(product, company)
 }
