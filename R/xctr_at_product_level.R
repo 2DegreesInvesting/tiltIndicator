e5628314eb887a1d38e1103f469ba64117da1ab9
Mauro Lepore <maurolepore@gmail.com>
2023-08-17 15:10:07 +0000

    rename

--- R/xctr_at_product_level.R
+++ R/xctr_at_product_level.R
@@ -5,6 +5,18 @@ xctr_at_product_level <- function(companies,
                                   low_threshold = 1 / 3,
                                   high_threshold = 2 / 3) {
   warn_product_and_company_level_functions_are_now_deprecated()
+  xctr_product(
+    companies =  companies,
+    co2 =  co2,
+    low_threshold =  low_threshold,
+    high_threshold =  high_threshold
+  )
+}
+xctr_product <- function(companies,
+                                  co2,
+                                  low_threshold = 1 / 3,
+                                  high_threshold = 2 / 3) {
+  warn_product_and_company_level_functions_are_now_deprecated()
   xctr_check(companies, co2)
 
   .companies <- prepare_companies(companies)
