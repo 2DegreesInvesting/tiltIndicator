fca18720ba3cd51c35ae6ce9f525c51ec9cd8747
Mauro Lepore <maurolepore@gmail.com>
2023-08-17 18:42:48 +0000

    rename

--- R/spui_product.R
+++ R/spui_product.R
@@ -1,8 +1,8 @@
 sector_profile_upstream_at_product_level <- function(companies,
-                         scenarios,
-                         inputs,
-                         low_threshold = ifelse(scenarios$year == 2030, 1 / 9, 1 / 3),
-                         high_threshold = ifelse(scenarios$year == 2030, 2 / 9, 2 / 3)) {
+                                                     scenarios,
+                                                     inputs,
+                                                     low_threshold = ifelse(scenarios$year == 2030, 1 / 9, 1 / 3),
+                                                     high_threshold = ifelse(scenarios$year == 2030, 2 / 9, 2 / 3)) {
   xstr_check(companies, scenarios)
 
   .companies <- prepare_companies(companies)
