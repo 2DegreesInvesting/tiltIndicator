f496c679878d26484f628ce19718e3fa9d035b72
Mauro Lepore <maurolepore@gmail.com>
2023-08-18 20:03:46 +0000

    Refactor

--- data/deprecated.R
+++ data/deprecated.R
@@ -1,6 +1,6 @@
 warn_deprecates <- function(new,
                             old,
-                            when = "tiltIndicator 0.0.0.9089",
+                            when,
                             package = "tiltToyData") {
   .Deprecated(msg = glue::glue(
     "`{old}` was deprecated in {when}.
@@ -15,7 +15,6 @@ read_toy_dataset <- function(dataset, ...) {
   readr::read_csv(path, show_col_types = FALSE, ...)
 }
 
-
 toy_ext <- function() {
   "csv.gz"
 }
@@ -26,18 +25,18 @@ toy_path <- function(file) {
 
 delayedAssign("companies", value = {
   "emissions_profile_any_companies" |>
-    warn_deprecates("companies") |>
+    warn_deprecates("companies", when = "tiltIndicator 0.0.0.9089") |>
     read_toy_dataset()
 })
 
 delayedAssign("products", value = {
   "emissions_profile_products" |>
-    warn_deprecates("products") |>
+    warn_deprecates("products", when = "tiltIndicator 0.0.0.9089") |>
     read_toy_dataset(col_types = readr::cols(isic_4digit = readr::col_character()))
 })
 
 delayedAssign("inputs", value = {
   "emissions_profile_upstream_products" |>
-    warn_deprecates("inputs") |>
+    warn_deprecates("inputs", when = "tiltIndicator 0.0.0.9089") |>
     read_toy_dataset(col_types = readr::cols(input_isic_4digit = readr::col_character()))
 })
