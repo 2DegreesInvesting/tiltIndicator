8c80ffc0ef598dd739a102908dbc7854dba25294
Mauro Lepore <maurolepore@gmail.com>
2023-08-22 15:50:13 +0000

    Document rename of datasets

--- R/rename.R
+++ R/rename.R
@@ -26,11 +26,11 @@
 #' * v0.0.0.9085: `istr()` -> `sector_profile_upstream()`
 #' * v0.0.0.9086: `xctr(companies, products)` -> `emissions_profile()`
 #' * v0.0.0.9087: `xctr(companies, inputs)` -> `emissions_profile_upstream()`
+#' * v0.0.0.9089: All datasets names match the functions above and moved to [tiltToyData](https://2degreesinvesting.github.io/tiltToyData/reference/index.html)
 #' * v0.0.0.9092: `xstr_pivot_type_sector_subsector()` -> `sector_profile_any_pivot_type_sector_subsector()`
 #' * v0.0.0.9092: `xstr_prepare_scenario()` -> `sector_profile_any_prepare_scenario()`
 #' * v0.0.0.9092: `xstr_prune_companies()` -> `sector_profile_any_prune_companies()`
 #' * v0.0.0.9092: `xstr_polish_output_at_company_level()` -> `sector_profile_any_polish_output_at_company_level()`
-#'
 #' @keywords internal
 #' @name rename
 #' @aliases NULL
