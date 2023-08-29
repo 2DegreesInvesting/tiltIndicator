273ff3c5dde83adf553baeb4a2d9152ecb5e5875
Mauro Lepore <maurolepore@gmail.com>
2023-08-29 16:27:52 +0000

    wip

--- tests/testthat/test-sector_profile_upstream_at_company_level.R
+++ tests/testthat/test-sector_profile_upstream_at_company_level.R
@@ -118,10 +118,10 @@ test_that("with type ipr, for each company and grouped_by value sums 1 (#216)", 
 })
 
 test_that("with type weo, for each company and grouped_by value sums 1 (#308)", {
-  .type <- "weo"
   companies <- example_companies()
-  scenarios <- read_test_csv(toy_sector_profile_any_scenarios(), n_max = Inf) |> filter(type == .type)
-  inputs <- read_test_csv(toy_sector_profile_upstream_products(), n_max = Inf) |> filter(type == .type)
+  .type <- "weo"
+  scenarios <- example_scenarios(!!aka("scenario_type") := .type)
+  inputs <- example_inputs(!!aka("scenario_type") := .type)
 
   product <- sector_profile_upstream_at_product_level(companies, scenarios, inputs)
   out <- any_at_company_level(product)
