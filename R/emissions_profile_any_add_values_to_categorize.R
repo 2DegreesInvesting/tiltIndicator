1047a57eb69becdb181d8407597e1f9811870cd0
Mauro Lepore <maurolepore@gmail.com>
2023-11-11 17:07:15 +0000

    Move it to it's own file

---
+++ R/emissions_profile_any_add_values_to_categorize.R
@@ -0,0 +1,13 @@
+emissions_profile_any_add_values_to_categorize <- function(data) {
+  add_rank <- function(data, .by) {
+    if (identical(.by, "all")) .by <- NULL
+    mutate(
+      data,
+      values_to_categorize = rank_proportion(.data[[find_co2_footprint(data)]]),
+      .by = all_of(.by)
+    )
+  }
+
+  benchmarks <- set_names(epa_benchmarks(), flat_benchmarks())
+  map_df(benchmarks, ~ add_rank(data, .x), .id = "grouped_by")
+}
--- R/emissions_profile_any_at_product_level.R
+++ R/emissions_profile_any_at_product_level.R
@@ -48,20 +48,6 @@ check_is_character <- function(x) {
   vec_assert(x, character())
 }
 
-emissions_profile_any_add_values_to_categorize <- function(data) {
-  add_rank <- function(data, .by) {
-    if (identical(.by, "all")) .by <- NULL
-    mutate(
-      data,
-      values_to_categorize = rank_proportion(.data[[find_co2_footprint(data)]]),
-      .by = all_of(.by)
-    )
-  }
-
-  benchmarks <- set_names(epa_benchmarks(), flat_benchmarks())
-  map_df(benchmarks, ~ add_rank(data, .x), .id = "grouped_by")
-}
-
 epa_benchmarks <- function() {
   list(
     "all",
