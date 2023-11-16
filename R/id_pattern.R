a5431cf94fe2f9da527717ce8718aa8de6943bb1
Mauro Lepore <maurolepore@gmail.com>
2023-11-16 19:30:41 +0000

    refactor

--- R/id_pattern.R
+++
@@ -1,7 +0,0 @@
-#' @examples
-#' grepl(id_pattern(), "companies_id")
-#' grepl(id_pattern(), "company_id")
-#' @noRd
-id_pattern <- function() {
-  "^compan.*_id$"
-}
--- R/utils-id.R
+++ R/utils-id.R
@@ -6,3 +6,11 @@ sanitize_id <- function(data) {
   }
   out
 }
+
+#' @examples
+#' grepl(id_pattern(), "companies_id")
+#' grepl(id_pattern(), "company_id")
+#' @noRd
+id_pattern <- function() {
+  "^compan.*_id$"
+}
