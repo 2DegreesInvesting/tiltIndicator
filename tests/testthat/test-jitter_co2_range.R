7e166c138f42f5b1c980837a27fb2dcdb15cc89c
Mauro Lepore <maurolepore@gmail.com>
2023-11-17 17:34:31 +0000

    refactor

--- tests/testthat/test-jitter_co2_range.R
+++ tests/testthat/test-jitter_co2_range.R
@@ -1,7 +1,8 @@
 test_that("if data lacks crucial columns, errors gracefully", {
   data <- emissions_profile(example_companies(), example_products()) |>
     unnest_product() |>
-    slice(1)
+    slice(1) |>
+    select(all_of(cols_crucial_jitter_co2_range(data)))
 
   crucial <- "grouped_by"
   bad <- select(data, -all_of(crucial))
