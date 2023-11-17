e0b33a349150dfaa39ab4f864e7bf3c1b567665e
Mauro Lepore <maurolepore@gmail.com>
2023-11-17 20:53:30 +0000

    wip

--- R/jitter_co2_range.R
+++ R/jitter_co2_range.R
@@ -2,17 +2,17 @@ jitter_co2_range <- function(data, amount = 0.1) {
   crucial <- c("grouped_by", "risk_category", aka("co2footprint"))
   walk(crucial, \(x) check_matches_name(data, x))
 
-  col <- co2_col <- find_co2_footprint(data)
+  co2_col <- find_co2_footprint(data)
 
-  without_missing_values <- data |>
-    sanitize_col("grouped_by") |>
-    sanitize_col("risk_category") |>
-    sanitize_col(co2_col)
+  no_na <- data |>
+    remove_na_from("grouped_by") |>
+    remove_na_from("risk_category") |>
+    remove_na_from(co2_col)
 
-  without_missing_values |>
+  no_na |>
     mutate(
-      lower = min(data[[co2_col]], na.rm = TRUE),
-      upper = max(data[[co2_col]], na.rm = TRUE),
+      lower = min(no_na[[co2_col]]),
+      upper = max(no_na[[co2_col]]),
       .by = c("grouped_by", "risk_category")
     ) |>
     distinct(grouped_by, risk_category, lower, upper) |>
@@ -22,7 +22,7 @@ jitter_co2_range <- function(data, amount = 0.1) {
     )
 }
 
-sanitize_col <- function(data, col) {
+remove_na_from <- function(data, col) {
   if (anyNA(data[[col]])) {
     warn_removing_na_from(data, col)
     data <- filter(data, !is.na(data[[col]]))
