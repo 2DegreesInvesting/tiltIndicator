ecb9a1baf191ad02d64fb3497e9783bf5418a88f
Mauro Lepore <maurolepore@gmail.com>
2023-11-17 20:47:30 +0000

    wip

--- R/jitter_co2_range.R
+++ R/jitter_co2_range.R
@@ -2,32 +2,14 @@ jitter_co2_range <- function(data, amount = 0.1) {
   crucial <- c("grouped_by", "risk_category", aka("co2footprint"))
   walk(crucial, \(x) check_matches_name(data, x))
 
-  sanitize_col <- function(data, col) {
-    if (anyNA(data[[col]])) {
-      warn_removing_na_from(data, col)
-      filter(data, !is.na(data[[col]]))
-    }
-  }
-
-  col <- "grouped_by"
-  if (anyNA(data[[col]])) {
-    warn_removing_na_from(data, col)
-    data <- filter(data, !is.na(data[[col]]))
-  }
-
-  col <- "risk_category"
-  if (anyNA(data[[col]])) {
-    warn_removing_na_from(data, col)
-    data <- filter(data, !is.na(data[[col]]))
-  }
-
   col <- co2_col <- find_co2_footprint(data)
-  if (anyNA(data[[col]])) {
-    warn_removing_na_from(data, col)
-    data <- filter(data, !is.na(data[[col]]))
-  }
 
-  data |>
+  without_missing_values <- data |>
+    sanitize_col("grouped_by") |>
+    sanitize_col("risk_category") |>
+    sanitize_col(co2_col)
+
+  without_missing_values |>
     mutate(
       lower = min(data[[co2_col]], na.rm = TRUE),
       upper = max(data[[co2_col]], na.rm = TRUE),
@@ -38,6 +20,14 @@ jitter_co2_range <- function(data, amount = 0.1) {
       lower_jitter = jitter_left(lower, amount),
       upper_jitter = jitter_right(upper, amount)
     )
+}
+
+sanitize_col <- function(data, col) {
+  if (anyNA(data[[col]])) {
+    warn_removing_na_from(data, col)
+    data <- filter(data, !is.na(data[[col]]))
+  }
+  data
 }
 
 warn_removing_na_from <- function(data, name) {
