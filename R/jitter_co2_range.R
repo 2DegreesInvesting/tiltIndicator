c4e343558fc637f7f55153bb1ab0e53c41ed3e7c
Mauro Lepore <maurolepore@gmail.com>
2023-11-17 17:30:30 +0000

    crucial

---
+++ R/jitter_co2_range.R
@@ -0,0 +1,6 @@
+jitter_co2_range <- function(data) {
+  crucial <- c("grouped_by", "risk_category", aka("co2footprint"))
+  check_crucial_names(data, crucial)
+
+  data
+}
---
+++ tests/testthat/test-jitter_co2_range.R
@@ -0,0 +1,9 @@
+test_that("if data lacks crucial columns, errors gracefully", {
+  data <- emissions_profile(example_companies(), example_products()) |>
+    unnest_product() |>
+    slice(1)
+
+  crucial <- "grouped_by"
+  bad <- select(data, -all_of(crucial))
+  expect_error(jitter_co2_range(bad), class = "missing_names")
+})
