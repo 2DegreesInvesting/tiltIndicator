440a9ced801b3535892cf03f9663600e84902b55
Mauro Lepore <maurolepore@gmail.com>
2023-11-17 19:59:04 +0000

    wip

--- R/jitter_co2_range.R
+++ R/jitter_co2_range.R
@@ -1,20 +1,31 @@
-jitter_co2_range <- function(data) {
+jitter_co2_range <- function(data, amount = 0.1) {
   crucial <- c("grouped_by", "risk_category", aka("co2footprint"))
   walk(crucial, \(x) check_matches_name(data, x))
 
-  data |>
-    add_co2_range()
-}
 
-add_co2_range <- function(data) {
   values <- get_column(data, find_co2_footprint(data))
-
   data |>
     mutate(
       lower = min(values),
       upper = max(values),
       .by = c("grouped_by", "risk_category")
     ) |>
-    distinct(grouped_by, risk_category, lower, upper)
+    distinct(grouped_by, risk_category, lower, upper) |>
+    mutate(
+      lower_jitter = jitter_left(lower, amount),
+      upper_jitter = jitter_right(lower, amount)
+    )
+}
+
+jitter_left <- function(x, amount = 0.1) {
+  x * (1 - absolute_jitter(x, amount))
+}
+
+jitter_right <- function(x, amount = 0.1) {
+  x * (1 + absolute_jitter(x, amount))
+}
+
+absolute_jitter <- function(x, amount = 0.1) {
+  abs(amount * rnorm(length(x)))
 }
 
