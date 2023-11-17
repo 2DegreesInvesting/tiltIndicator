1d2fabbe7a5e8a7543677edeac7b08112e84220a
Mauro Lepore <maurolepore@gmail.com>
2023-11-17 18:05:44 +0000

    wip

--- tests/testthat/test-jitter_co2_range.R
+++ tests/testthat/test-jitter_co2_range.R
@@ -1,8 +1,10 @@
-test_that("if data lacks crucial columns, errors gracefully", {
+test_that("with products, if data lacks crucial columns, errors gracefully", {
+  # styler: off
   data <- tribble(
     ~grouped_by, ~risk_category, ~co2_footprint,
           "all",         "high",            "1"
   )
+  # styler: on
 
   crucial <- "grouped_by"
   bad <- select(data, -all_of(crucial))
@@ -15,4 +17,25 @@ test_that("if data lacks crucial columns, errors gracefully", {
   crucial <- find_co2_footprint(data)
   bad <- select(data, -all_of(crucial))
   expect_error(jitter_co2_range(bad), crucial)
+})
+
+test_that("with upstream products, if data lacks crucial columns, errors gracefully", {
+  # styler: off
+  data <- tribble(
+    ~grouped_by, ~risk_category, ~input_co2_footprint,
+          "all",         "high",                  "1"
+  )
+  # styler: on
+
+  crucial <- "grouped_by"
+  bad <- select(data, -all_of(crucial))
+  expect_error(jitter_co2_range(bad), crucial)
+
+  crucial <- "risk_category"
+  bad <- select(data, -all_of(crucial))
+  expect_error(jitter_co2_range(bad), crucial)
+
+  crucial_pattern <- aka("co2footprint")
+  bad <- select(data, -matches(crucial_pattern))
+  expect_error(jitter_co2_range(bad), crucial_pattern)
 })
