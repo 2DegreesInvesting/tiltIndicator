ce77898b4649def0dd9d541d1e938c42336261c6
Mauro Lepore <maurolepore@gmail.com>
2023-11-18 14:11:55 +0000

    refactor

--- R/jitter_co2_range.R
+++ R/jitter_co2_range.R
@@ -1,32 +1,19 @@
 jitter_co2_range <- function(data, amount = 0.1) {
   check_jitter_co2_range(data)
 
-  with_range <- data |>
-    remove_missing_values_from_crucial_cols() |>
-    add_range_by(c("grouped_by", "risk_category"))
-
-  relevant <- with_range |>
-    distinct(.data$grouped_by, .data$risk_category, .data$lower, .data$upper)
-
-  relevant |>
-    mutate(
-      lower_jitter = jitter_left(.data$lower, amount),
-      upper_jitter = jitter_right(.data$upper, amount)
-    )
-
-  expand_jitter_range <- function(data, lower, upper, amount) {
-    data |>
-      mutate(
-        lower_jitter = jitter_left(lower, amount),
-        upper_jitter = jitter_right(upper, amount)
-      )
-  }
-
-  relevant |>
+  data |>
+    remove_missing_values_from_crucial_columns() |>
+    add_range_by(c("grouped_by", "risk_category")) |>
+    distinct(.data$grouped_by, .data$risk_category, .data$lower, .data$upper) |>
     expand_jitter_range(lower = .data$lower, upper = .data$upper, amount = 0.1)
 }
 
-remove_missing_values_from_crucial_cols <- function(data) {
+check_jitter_co2_range <- function(data) {
+  crucial <- c("grouped_by", "risk_category", aka("co2footprint"))
+  walk(crucial, \(x) check_matches_name(data, x))
+}
+
+remove_missing_values_from_crucial_columns <- function(data) {
   col_to_range <- find_co2_footprint(data)
 
   data |>
@@ -38,17 +25,24 @@ remove_missing_values_from_crucial_cols <- function(data) {
 add_range_by <- function(data, .by = c("grouped_by", "risk_category")) {
   col_to_range <- find_co2_footprint(data)
 
-  with_range <- data |>
-    mutate(
-      lower = min(data[[col_to_range]]),
-      upper = max(data[[col_to_range]]),
-      .by = all_of(.by)
-    )
+  with_range <- mutate(
+    data,
+    lower = min(data[[col_to_range]]),
+    upper = max(data[[col_to_range]]),
+    .by = all_of(.by)
+  )
 }
 
-check_jitter_co2_range <- function(data) {
-  crucial <- c("grouped_by", "risk_category", aka("co2footprint"))
-  walk(crucial, \(x) check_matches_name(data, x))
+pick_crucial_data <- function(data) {
+  distinct(data, .data$grouped_by, .data$risk_category, .data$lower, .data$upper)
+}
+
+expand_jitter_range <- function(data, lower, upper, amount) {
+  mutate(
+    data,
+    lower_jitter = jitter_left(lower, amount),
+    upper_jitter = jitter_right(upper, amount)
+  )
 }
 
 remove_na_from <- function(data, name) {
