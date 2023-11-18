c219e2cc66c2e07805862888377c9ecb1fee84ef
Mauro Lepore <maurolepore@gmail.com>
2023-11-18 14:40:57 +0000

    refactor

--- R/jitter_co2_range.R
+++ R/jitter_co2_range.R
@@ -3,26 +3,26 @@ jitter_co2_range <- function(data, amount = 0.1) {
 
   clean <- remove_missing_values_from_crucial_columns(data)
   x <- clean[[find_co2_footprint(clean)]]
-  .by <- c("grouped_by", "risk_category")
-
   clean |>
-    mutate(lower = min(x), upper = max(x), .by = all_of(.by)) |>
+    mutate(lower = min(x), upper = max(x), .by = all_of(cols_to_range_by())) |>
     distinct(.data$grouped_by, .data$risk_category, .data$lower, .data$upper) |>
     expand_jitter_range(lower = .data$lower, upper = .data$upper, amount = 0.1)
 }
 
 check_jitter_co2_range <- function(data) {
-  crucial <- c("grouped_by", "risk_category", aka("co2footprint"))
+  crucial <- c(cols_to_range_by(), aka("co2footprint"))
   walk(crucial, \(x) check_matches_name(data, x))
 }
 
 remove_missing_values_from_crucial_columns <- function(data) {
   col_to_range <- find_co2_footprint(data)
+  crucial <- c(cols_to_range_by(), col_to_range)
 
-  data |>
-    remove_na_from("grouped_by") |>
-    remove_na_from("risk_category") |>
-    remove_na_from(col_to_range)
+  for (i in seq_along(crucial)) {
+    data <- remove_na_from(data, crucial[[i]])
+  }
+
+  data
 }
 
 expand_jitter_range <- function(data, lower, upper, amount) {
