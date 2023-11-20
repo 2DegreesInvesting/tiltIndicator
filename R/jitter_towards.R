f931b9c3db4d7294ba59f2cc62c26c899356bf50
Mauro Lepore <maurolepore@gmail.com>
2023-11-20 11:16:46 +0000

    style

--- R/jitter_towards.R
+++ R/jitter_towards.R
@@ -1,11 +1,12 @@
 jitter_towards <- function(x, towards = c("left", "right"), amount = 0.1) {
   towards <- rlang::arg_match(towards)
-  sign <- switch (towards, left = `-`, right = `+`)
+  sign <- switch(towards,
+    left = `-`,
+    right = `+`
+  )
   sign(x, jitter_abs(x, amount))
 }
 
 jitter_abs <- function(x, amount = 0.1) {
   abs(amount * rnorm(length(x)))
 }
-
-
