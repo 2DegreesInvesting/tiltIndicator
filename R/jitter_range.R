79f3aea971d4ae4d5cf509718921e0aa91c4c4c9
Mauro Lepore <maurolepore@gmail.com>
2023-11-23 07:12:08 -0300

    * New helpres `summarize_range()` and `jitter_range()` (#622)

---
+++ R/jitter_range.R
@@ -0,0 +1,44 @@
+#' Expand a range adding some random noise
+#'
+#' This function expands a range by adding noise to the left of the minimum
+#' values and to the right of the maximum values.
+#'
+#' @param data A dataframe with columns `min` and `max`.
+#' @inheritParams base::jitter
+#'
+#' @family helpers
+#'
+#' @return The input dataframe with the additional columns `min_jitter` and
+#'   `max_jitter`.
+#' @export
+#'
+#' @examples
+#' library(tibble)
+#' set.seed(123)
+#'
+#' data <- tibble(min = -2:2, max = -1:3)
+#'
+#' data |> jitter_range(amount = 0.1)
+#'
+#' data |> jitter_range(amount = 2)
+jitter_range <- function(data, factor = 1, amount = NULL) {
+  check_crucial_names(data, c("min", "max"))
+
+  mutate(
+    data,
+    min_jitter = jitter_left(min, factor = factor, amount = amount),
+    max_jitter = jitter_right(max, factor = factor, amount = amount)
+  )
+}
+
+jitter_left <- function(x, factor, amount) {
+  x - jitter_abs(x, factor = factor, amount = amount)
+}
+
+jitter_right <- function(x, factor, amount) {
+  x + jitter_abs(x, factor = factor, amount = amount)
+}
+
+jitter_abs <- function(x, factor, amount) {
+  abs(abs(x) - abs(jitter(x, factor = factor, amount = amount)))
+}
---
+++ tests/testthat/test-jitter_range.R
@@ -0,0 +1,36 @@
+test_that("if `data` lacks crucial columns, errors gracefully", {
+  data <- tibble(bad = 1, max = 1)
+  expect_error(jitter_range(data), class = "missing_names")
+
+  data <- tibble(min = 1, bad = 1)
+  expect_error(jitter_range(data), class = "missing_names")
+})
+
+test_that("the jitter expands the range", {
+  data <- tibble(min = -100:100, max = -100:100)
+
+  out <- jitter_range(data)
+
+  expect_true(all(out$min_jitter < data$min))
+  expect_true(all(out$max_jitter > data$max))
+})
+
+test_that("is sensitive to `factor`", {
+  data <- tibble(min = 1:30, max = 1:30)
+
+  out1 <- jitter_range(data, factor = 0.1)
+  out2 <- jitter_range(data, factor = 0.9)
+
+  expect_true(mean(out2$min_jitter) < mean(out1$min_jitter))
+  expect_true(mean(out2$max_jitter) > mean(out1$max_jitter))
+})
+
+test_that("is sensitive to `amount`", {
+  data <- tibble(min = 1:30, max = 1:30)
+
+  out1 <- jitter_range(data, amount = 0.1)
+  out2 <- jitter_range(data, amount = 0.9)
+
+  expect_true(mean(out2$min_jitter) < mean(out1$min_jitter))
+  expect_true(mean(out2$max_jitter) > mean(out1$max_jitter))
+})
