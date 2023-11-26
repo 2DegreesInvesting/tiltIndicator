e6742bd9040d2c66587329dab8bd142671bfbdaa
Mauro Lepore <maurolepore@gmail.com>
2023-11-26 21:49:17 +0000

    New internal percent_noise()

--- R/jitter_range.R
+++ R/jitter_range.R
@@ -58,7 +58,27 @@ noise_other <- function(x, factor, amount) {
   abs(x * factor)
 }
 
+
+#' Calculate percent noise
+#'
+#' @param x Numeric vector.
+#' @param noisy Numeric vector.
+#'
+#' @return Numeric vector.
+#' @export
+#'
+#' @keywords internal
+#'
+#' @examples
+#' x <- -10:10
+#' noisy <- jitter(x)
+#' out <- percent_noise(x, noisy)
+#' out
+#'
+#' finite <- out[is.finite(out)]
+#' mean(finite)
 percent_noise <- function(x, noisy) {
   noise <- abs(abs(x) - abs(noisy))
   noise * 100 / x
 }
+
