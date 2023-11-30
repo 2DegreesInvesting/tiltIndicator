3d28a7fb9433e8008afbb54c610e4622b67611f5
Mauro Lepore <maurolepore@gmail.com>
2023-11-30 16:03:37 -0300

    Export internals useful in tiltIndicatorAfter (#632)
    * Export document_default_value() as internal
    * Export nest_levels() as internal

--- R/nest_levels.R
+++ R/nest_levels.R
@@ -1,3 +1,11 @@
+#' Nest levels
+#'
+#' @keywords internal
+#' @export
+#' @examples
+#' product <- data.frame(companies_id = 1, x = 1)
+#' company <- data.frame(companies_id = 1, x = 1)
+#' nest_levels(product, company)
 nest_levels <- function(product, company) {
   .by <- "companies_id"
   left_join(
