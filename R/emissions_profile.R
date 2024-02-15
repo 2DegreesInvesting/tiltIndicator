92299f6c2c9525ca4ff6f30aaf2d74e5ecc1fd96
Mauro Lepore <maurolepore@gmail.com>
2024-02-15 21:23:17 +0000

    refactor

--- R/emissions_profile.R
+++ R/emissions_profile.R
@@ -33,12 +33,12 @@ emissions_profile_upstream <- emissions_profile
 insert_row_with_na_in_risk_category <- function(data) {
   levels <- c(risk_category_levels(), NA)
   data |>
-    mutate(has_na = anyNA(risk_category), .by = grouped_by) |>
+    mutate(has_na = anyNA(.data$risk_category), .by = grouped_by) |>
     filter(!has_na) |>
-    distinct(companies_id, grouped_by) |>
-    dplyr::bind_cols(risk_category = NA_character_, value = 0) |>
+    distinct(.data$companies_id, .data$grouped_by) |>
+    dplyr::bind_cols(tibble::tibble(risk_category = NA_character_, value = 0)) |>
     bind_rows(data) |>
-    mutate(risk_category = factor(risk_category, levels)) |>
-    arrange(companies_id, grouped_by, risk_category) |>
-    mutate(risk_category = as.character(risk_category))
+    mutate(risk_category = factor(.data$risk_category, levels)) |>
+    arrange(.data$companies_id, .data$grouped_by, .data$risk_category) |>
+    mutate(risk_category = as.character(.data$risk_category))
 }
