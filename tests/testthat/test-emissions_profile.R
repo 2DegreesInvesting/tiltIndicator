13beb38a642d61911bab34e391fa08feb817cdfe
Mauro Lepore <maurolepore@gmail.com>
2024-02-22 13:15:54 +0000

    refactor

--- tests/testthat/test-emissions_profile.R
+++ tests/testthat/test-emissions_profile.R
@@ -353,7 +353,7 @@ test_that("at company level, 2 matched products yield `value = 1` in 1 `risk_cat
   expect_equal(sort(out), c(0, 0, 0, 1))
 })
 
-test_that("at company level, 1 matched and 1 unmatched products yield `value = 1/2` where `risk_category = NA` and in 1 other `risk_category` (#657)", {
+test_that("at company level, one matched and one unmatched products yield `value = 1/2` where `risk_category = NA` and in 1 other `risk_category` (#657)", {
   one_matched_one_unmatched <- c("a", "unmatched")
   companies <- example_companies(!!aka("uid") := one_matched_one_unmatched)
   matched <- one_matched_one_unmatched[1]
@@ -369,7 +369,7 @@ test_that("at company level, 1 matched and 1 unmatched products yield `value = 1
   expect_equal(sort(other), c(0, 0, 1 / 2))
 })
 
-test_that("at company level, 2 matched and 1 unmatched products yield `value = 1/3` where `risk_category = NA` and `value = 2/3` in 1 other `risk_category` (#657)", {
+test_that("at company level, two matched and one unmatched products yield `value = 1/3` where `risk_category = NA` and `value = 2/3` in 1 other `risk_category` (#657)", {
   two_matched_and_one_unmatched <- c("a", "b", "unmatched")
   companies <- example_companies(!!aka("uid") := two_matched_and_one_unmatched)
   matched <- two_matched_and_one_unmatched[1:2]
