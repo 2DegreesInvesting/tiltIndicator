6f83892d14a0c1737d89678f065b0552ed1c7b35
Mauro Lepore <maurolepore@gmail.com>
2024-04-02 18:31:14 +0000

    refactor

--- R/sector_profile.R
+++ R/sector_profile.R
@@ -20,30 +20,11 @@ sector_profile <- function(companies,
                            scenarios,
                            low_threshold = ifelse(scenarios$year == 2030, 1 / 9, 1 / 3),
                            high_threshold = ifelse(scenarios$year == 2030, 2 / 9, 2 / 3)) {
-  warn_custom_threshold(low_threshold)
-  warn_custom_threshold(high_threshold)
+  check_thresholds(low_threshold, high_threshold)
 
   product <- sector_profile_at_product_level(companies, scenarios, low_threshold, high_threshold)
   company <- epa_at_company_level(product) |>
     insert_row_with_na_in_risk_category()
 
   nest_levels(product, company)
-}
-
-warn_custom_threshold <- function(x) {
-  type <- gsub("^(.*)_.*$", "\\1", deparse(substitute(x)))
-  default <- unique(x) %in% thresholds()[[type]]
-  if (!default) {
-    rlang::warn(glue::glue("Using a non-default value of `{type}_threshold`: {x}."))
-  }
-
-  invisible(x)
-}
-
-thresholds <- function() {
-  tibble::tribble(
-    ~year, ~low, ~high,
-     2030,  1/9,   2/9,
-       NA,  1/3,   2/3,
-  )
 }
