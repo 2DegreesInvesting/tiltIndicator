a3bbaf45acc70bae82c50583163a597b566922b3
Mauro Lepore <maurolepore@gmail.com>
2024-04-02 19:55:38 +0000

    refactor

--- R/spa.R
+++ R/spa.R
@@ -46,40 +46,6 @@ prepare_scenarios <- function(data, low_threshold, high_threshold) {
     warn_non_default_thresholds()
 }
 
-warn_non_default_thresholds <- function(data, ...) {
-  warn_non_default_thresholds_impl(data, year == 2030, .threshold = "low_threshold")
-  warn_non_default_thresholds_impl(data, is.na(year) | year != 2030, .threshold = "low_threshold")
-  warn_non_default_thresholds_impl(data, year == 2030, .threshold = "high_threshold")
-  warn_non_default_thresholds_impl(data, is.na(year) | year != 2030, .threshold = "high_threshold")
-
-  invisible(data)
-}
-
-warn_non_default_thresholds_impl <- function(data, .threshold, ...) {
-  data$year <- as.integer(data$year)
-
-  thresholds <- distinct(data, year, low_threshold, high_threshold)
-  year_data <- filter(thresholds, ...)
-  if (nrow(year_data) == 1L) {
-    actual <- as.double(year_data[[.threshold]])
-    default <- as.double(filter(default_thresholds(), ...)[[.threshold]])
-    same <- identical(actual, default)
-    if (!same) {
-      rlang::warn(glue::glue("Using a non-default `{.threshold}`: {actual}."))
-    }
-  }
-}
-
-default_thresholds <- function() {
-  # styler: off
-  tibble::tribble(
-    ~year, ~low_threshold, ~high_threshold,
-     2030,            1/9,             2/9,
-       NA,            1/3,             2/3,
-  )
-  # styler: on
-}
-
 spa_compute_profile_ranking <- function(data, scenarios) {
   left_join(
     data, scenarios,
