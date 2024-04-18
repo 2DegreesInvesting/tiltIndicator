a207cec5c0e9102c013d1c92bdb25fe4ae5948e6
Mauro Lepore <maurolepore@gmail.com>
2024-04-18 16:53:19 -0300

    Standalone `check_matches_name()` now handles multiple matches (#763)

--- R/standalone-name-pattern.R
+++ R/standalone-name-pattern.R
@@ -1,5 +1,12 @@
+# ---
+# repo: 2DegreesInvesting/tiltIndicator
+# file: standalone-name-pattern.R
+# last-updated: 2024-04-18
+# license: https://unlicense.org
+# ---
+
 check_matches_name <- function(data, pattern) {
-  if (!matches_name(data, pattern)) {
+  if (!any(matches_name(data, pattern))) {
     abort(c(
       glue("The data lacks a column matching the pattern '{pattern}'."),
       i = "Are you using the correct data?"
