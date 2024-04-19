09c9e54a2047391a680b72141b4d654b80599206
Mauro Lepore <maurolepore@gmail.com>
2024-04-19 06:53:17 -0300

    * `summarize_range()` now also works with lists of data frames (#764)

--- R/summarize_range.R
+++ R/summarize_range.R
@@ -1,16 +1,21 @@
 #' Summarize the range of a column by groups
 #'
-#' This function is a shortcut to `dplyr::summarize(data, min = min(x), max =
-#' max(x))`.
+#' This funciton is a generic that includes support for dataframes and lists of
+#' dataframes. For dataframes, this function is a shortcut to
+#' `dplyr::summarize(data, min = min(x), max = max(x))`. For lists of dataframes
+#' that idea applies applies to each element of the list.
 #'
-#' @param data A dataframe.
+#' @param data A dataframe or a list of dataframes.
 #' @param col Unquoted expression giving the name of a column in `data`.
+#' @param .by A vector or list of vectors depending on whether `data` is a
+#' dataframe or list of dataframes, respective. For details see `.by` in
+#' [dplyr::summarize()].
 #' @inheritParams dplyr::summarize
 #' @inheritParams base::min
 #'
 #' @seealso [dplyr::summarize()]
 #'
-#' @return A dataframe:
+#' @return A dataframe or a list of dataframes:
 #'   * The rows come from the underlying groups.
 #'   * The columns come from the grouping keys plus the new columns `min` and
 #'   `max`.
@@ -23,15 +28,51 @@
 #' @examples
 #' library(tibble)
 #'
-#' data <- tibble(x = 1:4, group = c(1, 1, 2, 2))
+#' data <- tibble(x = 1:4, group = letters[c(1, 1, 2, 2)])
 #' data
 #'
-#' summarize_range(data, x, .by = group)
+#' summarize_range(data, "x", .by = "group")
+#'
+#' list <- split(data, data$group)
+#' list
+#'
+#' summarize_range(list, col = "x", .by = list(a = "group", b = "group"))
 summarize_range <- function(data, col, .by = NULL, na.rm = FALSE) {
+  UseMethod("summarize_range")
+}
+
+#' @export
+summarize_range.data.frame <- function(data, col, .by = NULL, na.rm = FALSE) {
+  msg <- "Passing `col` as a symbol is superseded. Use the string 'col' instead."
+  .col <- rlang::quo_get_expr(enquo(col))
+  if (is.symbol(.col)) {
+    warn(msg, class = "passing_col_as_a_symbol_is_superseded")
+    col <- rlang::as_name(.col)
+  }
+
   summarize(
     data,
-    min = min({{ col }}, na.rm = na.rm),
-    max = max({{ col }}, na.rm = na.rm),
+    min = min(.data[[col]], na.rm = na.rm),
+    max = max(.data[[col]], na.rm = na.rm),
     .by = {{ .by }}
   )
+}
+
+# TODO check that .x is a list
+# TODO check that .by is a named list
+# TODO check the relationship between the names of .x and .by
+#' @export
+summarize_range.list <- function(data, col, .by = NULL, na.rm = FALSE) {
+  out <- vector("list", length = length(data))
+  names(out) <- names(data)
+  for (i in names(data)) {
+    out[[i]] <- summarize_range(
+      data[[i]],
+      col = {{ col }},
+      .by = all_of(.by[[i]]),
+      na.rm = na.rm
+    )
+  }
+
+  out
 }
