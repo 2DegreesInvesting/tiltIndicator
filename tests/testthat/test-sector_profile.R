6289cd91c06ec473efeec0f58a80c183d2a13b13
Mauro Lepore <maurolepore@gmail.com>
2024-05-13 23:01:28 +0000

    Prune

--- tests/testthat/test-sector_profile.R
+++ tests/testthat/test-sector_profile.R
@@ -128,15 +128,7 @@ test_that("at company level, two matched and one unmatched products yield `value
   expect_equal(sort(other), c(0, 0, 2 / 3))
 })
 
-test_that("at product level, when `companies$tilt_sector` doesn't match `scenarios$sector` then `product$grouped_by` is `NA`", {
-  # This is what I think an analyst would say:
-  # > A product's tilt_sector with unmatched scenarios's sector yields a "missing benchmark"
-  #
-  # Translation to tiltIndicator's code:
-  # * given: the value "b" of `companies$clustered`
-  # * when: the corresponding value of `companies$tilt_sector` doesn't match any value of `scenarios$sector`
-  # * then: the result at product level has the value `NA` in `product$grouped_by`
-
+test_that("at product level, when `companies$sector` doesn't match `scenarios$sector` then `product$grouped_by` is `NA`", {
   # Based on this GoogleSheet row:
   # https://docs.google.com/spreadsheets/d/16u9WNtVY-yDsq6kHANK3dyYGXTbNQ_Bn/edit#gid=156243064&range=A5:I5
   # styler: off
@@ -155,7 +147,7 @@ test_that("at product level, when `companies$tilt_sector` doesn't match `scenari
   expect_true(is.na(product$grouped_by))
 })
 
-test_that("at product level, when `companies$tilt_sector` doesn't match `scenarios$sector` then `product$risk_category` and  `product$profile_ranking` are `NA`", {
+test_that("at product level, when `companies$sector` doesn't match `scenarios$sector` then `product$risk_category` and  `product$profile_ranking` are `NA`", {
   # TODO: DRY with the test above
   # styler: off
   companies <- tribble(
