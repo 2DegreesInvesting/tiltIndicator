4d8f46ae06bc619036a4d99ba73763a1b34a8311
Mauro Lepore <maurolepore@gmail.com>
2024-05-15 17:44:19 +0000

    Revert tests/ to main

--- tests/testthat/test-sector_profile.R
+++ tests/testthat/test-sector_profile.R
@@ -44,7 +44,7 @@ test_that("at product level, preserves unmatched products", {
   expect_true("unmatched" %in% out[[aka("uid")]])
 })
 
-test_that("at product level, an unmatched product yields `NA` in `risk_category` and `profile_ranking`", {
+test_that("at product level, unmatched product yield `NA` in the expected columns", {
   companies <- example_companies(
     !!aka("uid") := c("a", "unmatched"),
     !!aka("xsector") := c("total", "unmatched"),
@@ -55,6 +55,7 @@ test_that("at product level, an unmatched product yields `NA` in `risk_category`
     unnest_product() |>
     filter(.data[[aka("uid")]] == "unmatched")
 
+  expect_true(is.na(out$grouped_by))
   expect_true(is.na(out$risk_category))
   expect_true(is.na(out$profile_ranking))
 })
@@ -67,17 +68,17 @@ test_that("at company level, `risk_category` always has the value `NA` (#638)", 
   expect_true(anyNA(out$risk_category))
 })
 
-test_that("at company level with one company, a company with one unmatched product yields 1 row for each `risk_category`", {
+test_that("at company level with one company, a company with one unmatched product yields 1 row", {
   companies <- example_companies(
     !!aka("xsector") := "unmatched",
   )
   scenarios <- example_scenarios()
 
   out <- sector_profile(companies, scenarios) |> unnest_company()
-  expect_equal(nrow(out), 4)
+  expect_equal(nrow(out), 1)
 })
 
-test_that("at company level with two companies, a company with one unmatched product yields 1 row for each `risk_category`", {
+test_that("at company level with two companies, a company with one unmatched product yields 1 row", {
   companies <- example_companies(
     !!aka("id") := c("a", "unmatched"),
     !!aka("uid") := c("a", "unmatched"),
@@ -89,11 +90,10 @@ test_that("at company level with two companies, a company with one unmatched pro
     unnest_company() |>
     filter(.data[[aka("id")]] == "unmatched")
 
-  expect_equal(nrow(filter(out)), 4)
+  expect_equal(nrow(filter(out)), 1)
 })
 
 test_that("at company level, one matched and one unmatched products yield `value = 1/2` where `risk_category = NA` and in one other `risk_category` (#657)", {
-  skip("FIXME rething expected values at company level")
   companies <- example_companies(
     !!aka("uid") := c("a", "unmatched"),
     !!aka("xsector") := c("total", "unmatched"),
@@ -110,8 +110,8 @@ test_that("at company level, one matched and one unmatched products yield `value
   expect_equal(sort(other), c(0, 0, 1 / 2))
 })
 
+
 test_that("at company level, two matched and one unmatched products yield `value = 1/3` where `risk_category = NA` and `value = 2/3` in one other `risk_category` (#657)", {
-  skip("FIXME rething expected values at company level")
   companies <- example_companies(
     !!aka("uid") := c("a", "b", "unmatched"),
     !!aka("xsector") := c("total", "total", "unmatched"),
@@ -126,161 +126,4 @@ test_that("at company level, two matched and one unmatched products yield `value
   expect_equal(na, 1 / 3)
   other <- pull(filter(out, !is.na(risk_category)), value)
   expect_equal(sort(other), c(0, 0, 2 / 3))
-})
-
-test_that("at product level, when a single product matches by `sector`, `subsector`, and `type`, then `grouped_by` has the format `<type>_<scenario>_<year>`, and `risk_category` and `profile_ranking` are not `NA`", {
-  # styler: off
-  companies <- tribble(
-    ~companies_id, ~clustered, ~activity_uuid_product_uuid, ~tilt_sector, ~tilt_subsector,       ~type,     ~sector,  ~subsector,
-              "a",        "a",                         "a",          "a",             "a",       "ipr",     "total",    "energy",
-  )
-  # type, sector and subsector all match
-  scenarios <- tribble(
-    ~sector,   ~subsector,  ~year, ~reductions, ~type, ~scenario,
-    "total",     "energy",   2050,         1.0, "ipr",       "a",
-  )
-  # styler: on
-
-  product <- sector_profile(companies, scenarios) |> unnest_product()
-
-  expect_equal(product$grouped_by, "ipr_a_2050")
-  expect_false(is.na(product$risk_category))
-  expect_false(is.na(product$profile_ranking))
-})
-
-test_that("at product level, when a single product is unmatched by `type`, `sector`, or `subsector`, then `risk_category`, and `profile_ranking` are `NA`", {
-  # Starting with a matched dataset, to later create different kinds of unmatched datasets
-  companies <- tribble(
-    ~companies_id, ~clustered, ~activity_uuid_product_uuid, ~tilt_sector, ~tilt_subsector,       ~type,     ~sector,  ~subsector,
-              "a",        "b",                       "any",        "any",           "any",       "ipr",     "total",    "energy",
-  )
-
-  scenarios <- tribble(
-    ~sector,   ~subsector,  ~year, ~reductions, ~type, ~scenario,
-    "total",     "energy",   2050,         1.0, "ipr",       "a",
-  )
-  # styler: on
-
-  col <- "type"
-  companies1 <- companies
-  companies1[[col]] <- "unmatched"
-  product <- sector_profile(companies1, scenarios) |> unnest_product()
-  expect_true(is.na(product$risk_category))
-  expect_true(is.na(product$profile_ranking))
-
-  col <- "sector"
-  companies1 <- companies
-  companies1[[col]] <- "unmatched"
-  product <- sector_profile(companies1, scenarios) |> unnest_product()
-  expect_true(is.na(product$risk_category))
-  expect_true(is.na(product$profile_ranking))
-
-  col <- "subsector"
-  companies1 <- companies
-  companies1[[col]] <- "unmatched"
-  product <- sector_profile(companies1, scenarios) |> unnest_product()
-  expect_true(is.na(product$risk_category))
-  expect_true(is.na(product$profile_ranking))
-})
-
-test_that("at product level, when a single product matches by `sector` and `subsector` for only one of two values of `type`, then `grouped_by` still has at least one value for each of the two values of `type`", {
-  # styler: off
-  companies <- tribble(
-    ~companies_id, ~clustered, ~activity_uuid_product_uuid, ~tilt_sector, ~tilt_subsector,       ~type,     ~sector,  ~subsector,
-              "a",        "c",                 "unmatched",          "c",             "c",       "ipr", "land use",   "land use",
-              "a",        "c",                 "unmatched",          "c",             "c",       "weo",         NA,           NA
-  )
-  scenarios <- tribble(
-       ~sector,   ~subsector,  ~year, ~reductions, ~type, ~scenario,
-    "land use",   "land use",   2050,         0.3, "ipr",       "a"
-  )
-  # styler: on
-
-  product <- sector_profile(companies, scenarios) |> unnest_product()
-
-  extract_unique_type <- function(x) {
-    x |>
-      strsplit("_") |>
-      lapply(\(x) x[[1]]) |>
-      unlist() |>
-      unique()
-  }
-
-  # TODO: This expectation may become more specific based on Tilman's answer to
-  # https://github.com/2DegreesInvesting/tiltIndicator/pull/739#issuecomment-2110919379
-  expect_equal(extract_unique_type(product$grouped_by), c("ipr", "weo"))
-})
-
-test_that("at company level, when a single product matches by `sector`, `subsector`, and `type` then the `value` is 1 in a single `risk_category` different from `NA`", {
-  # styler: off
-  companies <- tribble(
-    ~companies_id, ~clustered, ~activity_uuid_product_uuid, ~tilt_sector, ~tilt_subsector,       ~type,     ~sector,  ~subsector,
-              "a",        "a",                         "a",          "a",             "a",       "ipr",     "total",    "energy",
-  )
-  # type, sector and subsector all match
-  scenarios <- tribble(
-    ~sector,   ~subsector,  ~year, ~reductions, ~type, ~scenario,
-    "total",     "energy",   2050,         1.0, "ipr",       "a",
-  )
-  # styler: on
-
-  company <- sector_profile(companies, scenarios) |> unnest_company()
-  values <- filter(company, !is.na(risk_category))$value
-  expect_equal(sort(values), c(0, 0, 1))
-})
-
-test_that("at company level, when a single product is unmatched by `type`, `sector`, or `subsector`, then the `value` is 1 where `risk_category` is `NA` and it is 0 for every other `risk_category`", {
-  # Starting with a matched dataset, to later create different kinds of unmatched datasets
-  companies <- tribble(
-    ~companies_id, ~clustered, ~activity_uuid_product_uuid, ~tilt_sector, ~tilt_subsector,       ~type,     ~sector,  ~subsector,
-              "a",        "b",                       "any",        "any",           "any",       "ipr",     "total",    "energy",
-  )
-
-  scenarios <- tribble(
-    ~sector,   ~subsector,  ~year, ~reductions, ~type, ~scenario,
-    "total",     "energy",   2050,         1.0, "ipr",       "a",
-  )
-  # styler: on
-
-  col <- "type"
-  companies1 <- companies
-  companies1[[col]] <- "unmatched"
-  company <- sector_profile(companies1, scenarios) |> unnest_company()
-  expect_equal(filter(company, is.na(risk_category))$value, 1L)
-  expect_equal(filter(company, !is.na(risk_category))$value, c(0, 0, 0))
-
-  col <- "sector"
-  companies1 <- companies
-  companies1[[col]] <- "unmatched"
-  company <- sector_profile(companies1, scenarios) |> unnest_company()
-  expect_equal(filter(company, is.na(risk_category))$value, 1L)
-  expect_equal(filter(company, !is.na(risk_category))$value, c(0, 0, 0))
-
-  col <- "subsector"
-  companies1 <- companies
-  companies1[[col]] <- "unmatched"
-  company <- sector_profile(companies1, scenarios) |> unnest_company()
-  expect_equal(filter(company, is.na(risk_category))$value, 1L)
-  expect_equal(filter(company, !is.na(risk_category))$value, c(0, 0, 0))
-})
-
-test_that("at company level, when a single product matches by `sector` and `subsector` for only one of two values of `type`, then `value` is 1 for the matched `type` in a single `risk_category` different from `NA`, and it is 1 for the unmatched `type` where `risk_category` is `NA`", {
-  # styler: off
-  companies <- tribble(
-    ~companies_id, ~clustered, ~activity_uuid_product_uuid, ~tilt_sector, ~tilt_subsector,       ~type,     ~sector,  ~subsector,
-              "a",        "c",                 "unmatched",          "c",             "c",       "ipr", "land use",   "land use",
-              "a",        "c",                 "unmatched",          "c",             "c",       "weo",         NA,           NA
-  )
-  scenarios <- tribble(
-       ~sector,   ~subsector,  ~year, ~reductions, ~type, ~scenario,
-    "land use",   "land use",   2050,         0.3, "ipr",       "a"
-  )
-  # styler: on
-
-  company <- sector_profile(companies, scenarios) |> unnest_company()
-  matched <- filter(company, startsWith(grouped_by, "ipr"), !is.na(risk_category))
-  expect_equal(sort(matched$value), c(0, 0, 1))
-
-  unmatched <- filter(company, startsWith(grouped_by, "weo"), is.na(risk_category))
-  expect_equal(unmatched$value, 1)
 })
