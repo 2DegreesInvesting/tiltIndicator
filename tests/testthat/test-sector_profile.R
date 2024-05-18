8d16b6f05b4bda73d334ff6513b09b33b8d1124d
Mauro Lepore <maurolepore@gmail.com>
2024-05-18 00:25:22 +0000

    refactor

--- tests/testthat/test-sector_profile.R
+++ tests/testthat/test-sector_profile.R
@@ -211,21 +211,8 @@ test_that("at company level, Tilman's example yields what he expects", {
 test_that("works with Tilman's example case 'a': match both", {
   # https://docs.google.com/spreadsheets/d/16u9WNtVY-yDsq6kHANK3dyYGXTbNQ_Bn/edit#gid=156243064
   # styler: off
-  companies_full <- tribble(
-    ~companies_id, ~clustered, ~activity_uuid_product_uuid, ~tilt_sector, ~tilt_subsector,       ~type,     ~sector,  ~subsector,
-              "a",        "a",                         "a",          "a",             "a",       "ipr",     "total",    "energy",
-              "a",        "a",                         "a",          "a",             "a",       "weo",     "total",    "energy",
-              "a",        "b",                 "unmatched",  "unmatched",     "unmatched", "unmatched", "unmatched", "unmatched",
-              "a",        "c",                 "unmatched",          "c",             "c",       "ipr",  "land use",  "land use",
-              "a",        "c",                 "unmatched",          "c",             "c",       "weo",          NA,          NA
-  )
-  scenarios <- tribble(
-         ~sector, ~subsector, ~year, ~reductions, ~type, ~scenario,
-         "total",   "energy",  2050,           1, "ipr",       "a",
-         "total",   "energy",  2050,         0.6, "weo",       "a",
-      "land use", "land use",  2050,         0.3, "ipr",       "a"
-  )
-  # styler: on
+  companies_full <- example_sector_companies()
+  scenarios <- example_sector_scenarios()
 
   # match both
   case <- "a"
